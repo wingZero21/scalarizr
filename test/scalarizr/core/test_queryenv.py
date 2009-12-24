ea52b1a200964bd30c2546e658683c8bf97ad077
Dmitry Korsakov <dmitry@scalr.com>
2009-12-24 15:51:43 +0000

    Implementation of test_list_roles function done. This part of test passed successfully.

--- test/scalarizr/core/test_queryenv.py
+++ test/scalarizr/core/test_queryenv.py
@@ -4,21 +4,35 @@ Created on Dec 23, 2009
 @author: marat
 '''
 import unittest
-
+import os
 
 class Test(unittest.TestCase):
 
-	_queryenv
+	_queryenv = None		
+
+	def setUp (self):
+		from scalarizr.core.queryenv import QueryEnvService
+		self._queryenv = QueryEnvService("", None, None)
 
 	def test_list_roles(self):
-		from xml.dom.minidom import getDOMImplementation
+		xmlfile = os.path.dirname(__file__) + "/../../resources/list_roles_response.xml"
 		
+		from xml.dom.minidom import parseString
+		xml = parseString(open(xmlfile, "r").read())
 		 
 		roles = self._queryenv._read_list_roles_response(xml)
 		
+		self.assertTrue(not roles is None)
 		self.assertEqual(len(roles), 3)
-		
-		pass
+		role = roles[0]
+		self.assertEqual(role.behaviour, "mysql")
+		self.assertEqual(role.name, "mysql-lvm")
+		hosts = role.hosts
+		self.assertTrue(hosts is not None)
+		host = hosts[0]
+		self.assertEqual(host.internal_ip, "211.31.14.198")
+		self.assertEqual(host.external_ip, "211.31.14.198")
+		self.assertTrue(host.replication_master)
 
 
 if __name__ == "__main__":
