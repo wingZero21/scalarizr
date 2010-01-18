b3c4e1bf9f14dca6caca398f565aa0f79732c0ca
Dmitry Korsakov <dmitry@scalr.com>
2010-01-18 09:03:02 +0000

    Some fixes in nginx_upstream_reload() method

--- src/scalarizr/core/handlers/nginx.py
+++ src/scalarizr/core/handlers/nginx.py
@@ -10,6 +10,8 @@ def get_handlers():
 	return [NginxHandler()]
 
 class NginxHandler(Handler):
+	_logger = None
+	_queryenv = None
 	
 	def on_HostUp(self, message):
 		self.nginx_upstream_reload()
@@ -18,36 +20,42 @@ class NginxHandler(Handler):
 		self.nginx_upstream_reload()
 	
 	def nginx_upstream_reload(self):
-		import cPickle
 		import os
 		import shutil
-		import ConfigParser
-		config = ConfigParser.RawConfigParser()
-		config.read("etc/include/handler.nginx.ini")
+		from scalarizr.core import Bus, BusEntries
+		import logging
+		import subprocess
+		
+		bus = Bus()
+		self._logger = logging.getLogger(__package__ + "." + self.__class__.__name__)
+		self._queryenv = Bus()[BusEntries.QUERYENV_SERVICE]
+		#import ConfigParser
+		#config = ConfigParser.RawConfigParser()
+		#config.read("etc/include/handler.nginx.ini")
+		config = bus[BusEntries.CONFIG]
 		nginx_bin = config.get("handler_nginx","binary_path")
 		nginx_incl = config.get("handler_nginx","app_include_path")
 		if config.get("handler_nginx","app_port"):
 			app_port = config.get("handler_nginx","app_port")
 		else:
 			app_port = "80"
+			
 		tmp_incl = ""
 		num_of_appservers = 0
-		if os.path.isfile("/usr/local/aws/templates/app-servers.tmpl"):
+		template_file = bus[BusEntries.BASE_PATH]+"/etc/include/handler.nginx/app-servers.tpl"
+		if os.path.isfile(template_file):
 			upstream_hosts = ""
-			# At first there need to call list_roles method
-			ec2_listhosts_app = None
-			basename_app_serv = None
+			ec2_listhosts_app = self._queryenv.list_roles("","app")
 			
 			for app_serv in ec2_listhosts_app :
-				upstream_hosts += "\tserver" + basename_app_serv + ":" + app_port + ";\n"
+				upstream_hosts += "\tserver" + ec2_listhosts_app.hosts.internal_ip + ":" + app_port + ";\n"
 				num_of_appservers = num_of_appservers + 1
 			
 			if 0 == num_of_appservers :
 				upstream_hosts = "\tserver 127.0.0.1:80;"
 			
 			if "" != upstream_hosts:
-				#with open(os.environ['TMP_INCL'], 'w') as fo: for line in open('/usr/local/aws/templates/app-servers.tmpl', 'r'): fo.write(line.replace('@@UPSTREAM_HOSTS@@', os.environ.get('UPSTREAM_HOSTS')))
-				tmp_incl = cPickle.load(file("/usr/local/aws/templates/app-servers.tmpl",'r'))
+				tmp_incl = open(template_file,'r').read()
 				tmp_incl = tmp_incl.replace("@@UPSTREAM_HOSTS@@",upstream_hosts)
 		else:
 			tmp_incl += "upstream backend {" + "\tip_hash;\n"
@@ -57,31 +65,31 @@ class NginxHandler(Handler):
 			if 0 == num_of_appservers : 
 				tmp_incl += "\tserver 127.0.0.1:80;"
 			tmp_incl = tmp_incl + "}"
-		#HTTPS Configuration
-		
-		if os.path.isfile("/etc/nginx/https.include") & os.path.isfile("/etc/aws/keys/ssl/https.key") :
+			
+		#HTTPS Configuration		
+		if os.path.isfile("/etc/nginx/https.include") & \
+		os.path.isfile("/etc/aws/keys/ssl/https.key") & \
+		os.path.isfile("/etc/aws/keys/ssl/https.cert") :
 			#Needs one more file checking (cert)? See in original code line 80
 			tmp_incl += "include /etc/nginx/https.include;"
 		#Determine, whether configuration was changed or no
-		if tmp_incl == cPickle.load(file(nginx_incl,'r')):
-			#log "nginx upstream configuration wasn`t changed."
+		if tmp_incl == open(nginx_incl,'r').read():
+			self._logger.info("nginx upstream configuration wasn`t changed.")
 			pass
 		else:
-			#log "nginx upstream configuration changed."
+			self._logger.info("nginx upstream configuration changed.")
 			shutil.move(nginx_incl, nginx_incl+".save")
 			shutil.move(tmp_incl, nginx_incl)
-			#log "Testing new configuration."
-			if os.path.isfile(nginx_bin): #&& NG_LOG=`$NGINX_BIN -t 2>&1`
-				#log "Configuration error detected: '$NG_LOG'. Reverting configuration."
+			self._logger.info("Testing new configuration.")
+			if os.path.isfile(nginx_bin) & (not subprocess.call([nginx_bin, "-t"])):
+				self._logger.error("Configuration error detected: '$NG_LOG'. Reverting configuration.")
 				shutil.move(nginx_incl, nginx_incl+".junk")
 				shutil.move(nginx_incl+".save", nginx_incl)
 			elif os.path.isfile("/var/run/nginx.pid"):
-				#log "Reloading nginx."
-				os.system("kill -HUP "+ cPickle.load(file("/var/run/nginx.pid",'r')))
+				self._logger.info("Reloading nginx.")
+				os.system("kill -HUP "+ open("/var/run/nginx.pid",'r').read())
 		#call_user_code lib/nginx_reload
 				
-			
-
 	
 	def accept(self, message, queue, behaviour=None, platform=None, os=None, dist=None):
 		return behaviour == "app" and (message.name == "HostUp" or message.name == "HostDown")	
