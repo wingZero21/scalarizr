9bdcd387ce8d69b72a72f28e37f00eceab7ee6b4
Dmitry Korsakov <dmitry@scalr.com>
2010-01-21 14:19:29 +0000

    method nginx_reload() and test for it both doing well. Just need some additional errors catching in nginx execution.

--- src/scalarizr/core/handlers/nginx.py
+++ src/scalarizr/core/handlers/nginx.py
@@ -42,14 +42,13 @@ class NginxHandler(Handler):
 			
 		tmp_incl = ""
 		num_of_appservers = 0
-		template_file = bus[BusEntries.BASE_PATH]+"/etc/include/handler.nginx/app-servers.tpl"
+		template_file = bus[BusEntries.BASE_PATH]+"/etc/include/handler.nginx/app-servers.tplm"
 		if os.path.isfile(template_file):
 			upstream_hosts = ""
 			ec2_listhosts_app = self._queryenv.list_roles(behaviour = "app")
-			
 			for app_serv in ec2_listhosts_app :
 				for app_hosts in app_serv.hosts :
-				    upstream_hosts += "\tserver" + app_hosts.internal_ip + ":" + app_port + ";\n"
+				    upstream_hosts += "\tserver " + app_hosts.internal_ip + ":" + app_port + ";\n"
 				    num_of_appservers = num_of_appservers + 1
 			
 			if 0 == num_of_appservers :
@@ -57,40 +56,67 @@ class NginxHandler(Handler):
 			
 			if "" != upstream_hosts:
 				tmp_incl = open(template_file,'r').read()
-				tmp_incl = tmp_incl.replace("@@UPSTREAM_HOSTS@@",upstream_hosts)
+				tmp_incl = tmp_incl.replace("${upstream_hosts}",upstream_hosts)
+
 		else:
 			tmp_incl += "upstream backend {" + "\tip_hash;\n"
+			ec2_listhosts_app = self._queryenv.list_roles(behaviour = "app")
 			for app_serv in ec2_listhosts_app : 
-				tmp_incl += "\tserver" + ":" + app_port + ";"
-				num_of_appservers = num_of_appservers + 1
+				for app_hosts in app_serv.hosts :
+					tmp_incl += "\tserver " + app_hosts.internal_ip + ":" + app_port + ";\n"
+					num_of_appservers = num_of_appservers + 1
 			if 0 == num_of_appservers : 
 				tmp_incl += "\tserver 127.0.0.1:80;"
 			tmp_incl = tmp_incl + "}"
-			
+		
 		#HTTPS Configuration		
+		# openssl req -new -x509 -days 9999 -nodes -out cert.pem -keyout cert.key
 		if os.path.isfile("/etc/nginx/https.include") and \
-		os.path.isfile("/etc/aws/keys/ssl/https.key") and \
-		os.path.isfile("/etc/aws/keys/ssl/https.cert") :
-			#Needs one more file checking (cert)? See in original code line 80
+		os.path.isfile("/etc/aws/keys/ssl/cert.key") and \
+		os.path.isfile("/etc/aws/keys/ssl/cert.pem") :
 			tmp_incl += "include /etc/nginx/https.include;"
+			
 		#Determine, whether configuration was changed or no
-		if tmp_incl == open(nginx_incl,'r').read():
+		if os.path.isfile(nginx_incl) and tmp_incl == open(nginx_incl,'r').read():
 			self._logger.info("nginx upstream configuration wasn`t changed.")
-			pass
 		else:
 			self._logger.info("nginx upstream configuration changed.")
-			shutil.move(nginx_incl, nginx_incl+".save")
-			shutil.move(tmp_incl, nginx_incl)
+			if os.path.isfile(nginx_incl):
+				shutil.move(nginx_incl, nginx_incl+".save")
+			file = open(nginx_incl, "w")
+			file.write(tmp_incl)
+			file.close()
 			self._logger.info("Testing new configuration.")
-			if os.path.isfile(nginx_bin) and (not subprocess.call([nginx_bin, "-t"])):
+			nginx_pid = "/var/run/nginx.pid"
+			
+			if os.path.isfile(nginx_bin) and subprocess.call([nginx_bin, "-t"]):
 				self._logger.error("Configuration error detected: '$NG_LOG'. Reverting configuration.")
-				shutil.move(nginx_incl, nginx_incl+".junk")
-				shutil.move(nginx_incl+".save", nginx_incl)
-			elif os.path.isfile("/var/run/nginx.pid"):
+				if os.path.isfile(nginx_incl):
+					shutil.move(nginx_incl, nginx_incl+".junk")
+				if os.path.isfile(nginx_incl+".save"):
+					shutil.move(nginx_incl+".save", nginx_incl)
+			elif os.path.isfile(nginx_bin) and os.path.isfile(nginx_pid):
 				self._logger.info("Reloading nginx.")
-				os.system("kill -HUP "+ open("/var/run/nginx.pid",'r').read())
-		#call_user_code lib/nginx_reload
+				#os.system("kill -HUP "+ open(nginx_pid,'r').read())
+				import subprocess
+				nginx_restart_command = "kill -HUP "+ open(nginx_pid,'r').read()
+				proc = subprocess.Popen(nginx_restart_command,
+                       shell=True,
+                       stdin=subprocess.PIPE,
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE,
+                       )
+				stdout_value, stderr_value = proc.communicate('through stdin to stdout')
+				print '\tpass through:', repr(stdout_value)
+				print '\tstderr:', repr(stderr_value)
 				
+				
+			elif not os.path.isfile(nginx_bin):
+				self._logger.error("Nginx not found.")
+				print nginx_bin
+			elif not os.path.isfile(nginx_pid):
+				self._logger.debug("/var/run/nginx.pid does not exist. Probably nginx haven`t been started")
+		#call_user_code lib/nginx_reload
 	
 	def accept(self, message, queue, behaviour=None, platform=None, os=None, dist=None):
 		return behaviour == "app" and (message.name == "HostUp" or message.name == "HostDown")	
--- test/scalarizr/core/handlers/test_nginx.py
+++ test/scalarizr/core/handlers/test_nginx.py
@@ -9,9 +9,40 @@ import unittest
 class Test(unittest.TestCase):
 
 
-    def testName(self):
-        pass
-
+    def test_nginx_upstream_reload(self):
+        from scalarizr.core import Bus, BusEntries
+        bus = Bus()
+        config = bus[BusEntries.CONFIG]
+        
+        import ConfigParser
+        from scalarizr.util import inject_config
+        c_parser = ConfigParser.ConfigParser()
+        c_parser.read('/home/shaitanich/workspace/scalarizr-trunk/etc/include/handler.nginx.ini')
+        
+        import sys
+        if sys.platform.startswith("freebsd"):
+            c_parser.set("handler_nginx", "binary_path", "/usr/local/sbin/nginx")
+            print c_parser.get("handler_nginx","binary_path")
+            
+        inject_config(config, c_parser)
+        
+        class _Bunch(dict):
+            __getattr__, __setattr__ = dict.get, dict.__setitem__
+            
+        class _QueryEnv:
+            def list_roles(self, behaviour):
+                return [_Bunch(
+                    behaviour = "app",
+                    name = "nginx",
+                    hosts = [_Bunch(index='1',replication_master="1",internal_ip="127.0.0.1",external_ip="192.168.1.93")]
+                    )]
+            
+        bus[BusEntries.QUERYENV_SERVICE] = _QueryEnv()
+        
+        from scalarizr.core.handlers import nginx
+        
+        n = nginx.NginxHandler()
+        n.nginx_upstream_reload()
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
