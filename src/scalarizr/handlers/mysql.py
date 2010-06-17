29e410c38cc3db2d1306d761eb3374feaf38ea7f
Nick Demyanchuk <spike@scalr.com>
2010-06-17 13:18:57 +0000

    Mysql handler implementation

--- src/scalarizr/handlers/mysql.py
+++ src/scalarizr/handlers/mysql.py
@@ -5,9 +5,9 @@ Created on 14.06.2010
 '''
 from scalarizr.bus import bus
 from scalarizr.behaviour import Behaviours
-from scalarizr.handlers import Handler, HandlerError
-from scalarizr.util import fstool, system
-import logging, os
+from scalarizr.handlers import Handler
+from scalarizr.util import fstool, system, cryptotool
+import logging, os, re, shutil
 
 def get_handlers ():
 	return [MysqlHandler()]
@@ -37,27 +37,26 @@ class MysqlHandler(Handler):
 		bus.on("before_host_up", self.on_before_host_up)
 		
 	def on_before_host_up(self, message):
-		role_name = None
 		config = bus.config
 		role_name = config.get('general','role_name')
 		role_params = self._queryenv.list_role_params(role_name)
 		if role_params["mysql_data_storage_engine"]:
 			# Poneslas' pizda po ko4kam
 			if "master" == role_name:
-				vol_id = role_params["mysql_ebs_vol_id"]
+				volId = role_params["mysql_ebs_vol_id"]
 				devname = '/dev/sdo'
 				ec2connection = self._platform.new_ec2_conn()
 				# Attach ebs
-				ebs_volumes = ec2connection.get_all_volumes([vol_id])
+				ebsVolumes = ec2connection.get_all_volumes([volId])
 				
-				if len(ebs_volumes):
-					ebs_volume = ebs_volumes[0]
-					if ebs_volume.volume_state() == 'available':
-						ec2connection.attach_volume(vol_id, self._iid, devname)
-#						while ebs_volume.attachment_state() != 'attached':
+				if 1 == len(ebsVolumes):
+					ebsVolume = ebsVolumes[0]
+					if ebsVolume.volume_state() == 'available':
+						ec2connection.attach_volume(volId, self._iid, devname)
+#						while ebsVolume.attachment_state() != 'attached':
 				else:
-					self._logger.error('Cannot find EBS volume (volume_id: %s)', vol_id)
-					return
+					self._logger.error('Can\'t find volume with ID =  %s ', volId)
+					raise
 							
 				#TODO: check if the volume has been successfully attached 
 				# Mount ebs # fstool.mount()
@@ -65,55 +64,103 @@ class MysqlHandler(Handler):
 			
 			elif "slave" == role_name or "eph" == role_params["mysql_data_storage_engine"]:
 				try:
-					devname = '/dev/' + self._platform.get_block_device_mapping()["ephemeral0"]
+					devname = '/dev/' + self._platform._fetch_ec2_meta('latest/meta-data/block-device-mapping/ephemeral0')
 				except Exception, e:
-					self._logger.error('Cannot retrieve device %s info: %s', devname, e)
+					self._logger.error('Can\'t retrieve device %s info: %s', devname, e)
 					raise
 								
 				self._mount_device(devname)	
 			
 			if not os.path.exists('/mnt/mysql-data') and not os.path.exists('/mnt/mysql-misc'):
-				# Stop
+				# Stop mysql server
 				init_script		= '/etc/init.d/mysql'
 				reload_command	= [init_script, 'stop']
 				if os.path.exists(init_script) and os.access(init_script, os.X_OK):
 					self._logger.info("Trying to stop mysql server..")
 					try:
-						# TODO(marat) place this into service_stop function
 						out, err, retcode = system(reload_command, shell=False)
 						if retcode or (out and out.find("FAILED") != -1):
 							self._logger.error("Mysql stopping failed. %s", out)
 						else:
-							self._logger.info("Mysql was successfully stopped")
-						# TODO: check that mysqld is really stopped
-						# /var/run/mysqld.pid
-							
+							self._logger.info("Mysql was successfully stopped.")
 					except OSError, e:
 						self._logger.error('Mysql stopping failed by running %s. %s',
 						''.join(reload_command), e.strerror)
-
-				try:
-					file = open('/etc/mysql/my.cnf', 'r')
-				except IOError, e:
-					self._logger.error('Cannot open /etc/mysql/my.cnf: %s', e.strerror )
-					raise
-				else:
-					myCnf = file.readlines()
-					file.close
-					
-								
+				
 				# Move datadir to EBS 
-				# /mnt/mysql-data
+				self._change_mysql_dir('log_bin', '/mnt/mysql-misc')
+				self._change_mysql_dir('datadir', '/mnt/mysql-data')
+				
 				# Start --skip-grant-tables
+				daemon = '/usr/sbin/mysqld'
+				start_command	= [daemon , '--skip-grant-tables']
+				if os.path.exists(daemon) and os.access(daemon, os.X_OK):
+					self._logger.info("Starting mysql server without grant tables.")
+					try:
+						out, err, retcode = system(start_command, shell=False)
+						if retcode or (out and out.find("ERROR") != -1):
+							self._logger.error("Mysql start failed. %s", out)
+						else:
+							self._logger.info("Mysql was successfully started")
+					except OSError, e:
+						self._logger.error('Mysql stopping failed by running %s. %s',
+						''.join(reload_command), e.strerror)
+				
 				# Add root user
+				mysqlClient = '/usr/bin/mysql'
+				password = re.sub('[\n=]','', cryptotool.keygen(32))
+				command = "INSERT INTO mysql.user VALUES('%','scalarizr',PASSWORD('"+password+"')" + ",'Y'"*28 + ",''"*4 +',0'*4+"); FLUSH PRIVILEGES;"
+				start_command	= [mysqlClient , '-e ' + command ]
+				try:
+					out, err, retcode = system(start_command, shell=False)
+					if retcode or (out and out.find("ERROR") != -1):
+						self._logger.error("Cannot add mysql user 'scalarizr': %s", out)
+					else:
+						self._logger.info("Mysql user 'scalarizr' successfully added.")
+				except OSError, e:
+					self._logger.error("Mysql user 'scalarizr' adding failed by running %s. %s",
+					''.join(reload_command), e.strerror)
+				
 				# Save root user to /etc/scalr/private.d/behaviour.mysql.ini
-				pass
-		
+				config.set('behaviour_mysql', "mysql_password", password)
 			if "master":
 				message.mysql_repl_user = ""
 				message.mysql_repl_password = ""
-				mtab = fstool.Mtab()
-				fstab = fstool.Fstab()	
+
+	
+	def _change_mysql_dir(self, directive=None, dirname = None):
+		if dirname and directive:
+			try:
+				file = open('/etc/mysql/my.cnf', 'r')
+			except IOError, e:
+				self._logger.error('Can\'t open /etc/mysql/my.cnf: %s', e.strerror )
+				raise
+			else:
+				myCnf = file.readlines()
+				file.close
+			
+			searchRow = re.compile('(^\s*'+directive+'\s*=\s*)((/[\w-]+)+)[/\s](/[\w-]+\.\w+)?', re.MULTILINE)	
+			srcDirRow = re.search(searchRow, myCnf)
+			if srcDirRow:
+				srcDir = srcDirRow.group(2)
+				if os.path.isdir(srcDir):
+					self._logger.info('Copying mysql directory \'%s\' to \'%s\'', srcDir, dirname)
+					shutil.copytree(srcDir, dirname)
+					myCnf = re.sub(searchRow, '\\1'+ dir + '\n' , myCnf)
+				else:
+					self._logger.error('Mysql directory \'%s\' doesn\'t exist. Creating new in \'%s\'', srcDir, dirname)
+					myCnf = re.sub(searchRow, '' , myCnf)
+					os.makedirs(dirname)
+					myCnf += '\n' + directive +' = ' + dirname
+
+			else:
+				os.makedirs(dir)
+				myCnf += '\n' + directive +' = ' + dir
+				
+			file = open('/etc/mysql/my.cnf', 'w')
+			file.write(myCnf)
+			file.close()
+		
 	
 	def _mount_device(self, devname):
 		fstab = fstool.Fstab()			
@@ -124,13 +171,12 @@ class MysqlHandler(Handler):
 					fstool.mount(devname, '/mnt', ["-t auto"])
 					break
 				except fstool.FstoolError, e:
-					if e.code == fstool.FstoolError.NO_FS:
+					if -666 == e.code:
 						system("/sbin/mkfs.ext3 -F " + devname + " 2>&1")
 					else:
-						self._logger.error('Cannot mount device %s : %s', devname, e)
+						self._logger.error('Can\'t mount device %s : %s', devname, e)
 						raise
 					
 		if not fstab.contains(devname, rescan=True):
 			self._logger.info("Adding a record to fstab")
 			fstab.append(fstool.TabEntry(devname, '/mnt', "auto", "defaults\t0\t0"))
-	
