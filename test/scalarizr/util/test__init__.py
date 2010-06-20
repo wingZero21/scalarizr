9720dd1153fb3dd5ecb15abe16529e31b61532f2
Dmitry Korsakov <dmitry@scalr.com>
2010-06-20 11:31:42 +0000

    added an example of "database is locked" error generation

--- test/scalarizr/util/test__init__.py
+++ test/scalarizr/util/test__init__.py
@@ -13,8 +13,24 @@ class TestSQLite(unittest.TestCase):
 
 	def setUp(self):
 		self.localobj = SqliteLocalObject(self._db_connect)
+		#for loop test:
+		conn = self.localobj.get().get_connection()
+		cur = conn.cursor()
+		sql = """create table test (key INTEGER PRIMARY KEY AUTOINCREMENT, data TEXT);"""
+		cur.execute(sql)
+		conn.commit()
+		#conn.close()
+		
 		
 	def tearDown(self):
+		#for loop test:
+		conn = self.localobj.get().get_connection()
+		cur = conn.cursor()
+		sql = """drop table test;"""
+		cur.execute(sql)
+		conn.commit()
+		conn.close()
+		#normal:
 		del self.localobj
 	
 	def test_get_from_the_same_thread(self):
@@ -22,16 +38,19 @@ class TestSQLite(unittest.TestCase):
 		conn2 = self.localobj.get().get_connection()
 		print conn1, conn2, "got from the same thread"
 		self.assertEqual(conn1, conn2)	
-		
+	"""	
 	def _test_get_from_the_same_thread2(self):
 		conn1 = self.localobj.get()
 		conn2 = self.localobj.get()
 		self.assertEqual(conn1.get_connection(), conn2.get_connection())
-
+	"""
+	
 	def _db_connect(self):
 		logger = logging.getLogger(__name__)
-		logger.info("Open SQLite database in memory")
-		conn = sqlite.Connection(":memory:")
+		#logger.info("Open SQLite database in memory")
+		#conn = sqlite.Connection(":memory:")
+		logger.info("Open SQLite database in file")
+		conn = sqlite.Connection("/home/shaitanich/workspace/test_db.db")
 		conn.row_factory = sqlite.Row
 		self.o_thread = conn
 		return conn		
@@ -44,8 +63,42 @@ class TestSQLite(unittest.TestCase):
 		t.start()
 		t.join()
 		print o_main, self.o_thread, "got from different threads"
-		self.assertNotEqual(o_main, self.o_thread) 
+		self.assertNotEqual(o_main, self.o_thread)
+
+	def _loop(self):
+		conn2 = self.localobj.get().get_connection()
+
+		for i in range(100):
+			try:
+				cur2 = conn2.cursor()
+				sql2 = """INSERT INTO test (data) VALUES ('trololo');"""
+				sqlsel2 = """select COUNT(*) from test where data = 'trololo';"""
+				cur2.execute(sql2)
+				cur2.execute(sqlsel2)
+				print "Trololo count: ", cur2.fetchone()[0]
+				conn2.commit()
+			except BaseException, e:
+				print "Trololo: ", str(e)
+		#conn2.close()
+	
+	def test_loop(self):
+		#for loop test:
+		conn = self.localobj.get().get_connection()
+		t = threading.Thread(target=self._loop)
+		t.start()
+		for i in range(100):
+			cur = conn.cursor()
+			sql = """INSERT INTO test (data) VALUES ('olololo');"""
+			sqlsel = """select COUNT(*) from test where data = 'olololo';"""
+			cur.execute(sql)
+			cur.execute(sqlsel)
+			print "Ololo count: ", cur.fetchone()[0]
+			conn.commit()
+		t.join()
+		#conn.close()
 		
+	
+
 class _SQLiteConnection(object):
 	_conn = None
 
@@ -58,7 +111,7 @@ class _SQLiteConnection(object):
 			self._conn = conn
 			
 		return self._conn
-		
+
 		
 	
 if __name__ == "__main__":
