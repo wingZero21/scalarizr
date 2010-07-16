9d511ad81d1b4b41bf28d627959c830cf8f6652b
Dmitry Korsakov <dmitry@scalr.com>
2010-07-16 15:33:25 +0000

    Fixed some problems with opened file desriptors. app-servers.conf file now included (btw in a wrong way) into nginx main config file.

--- src/scalarizr/handlers/nginx.py
+++ src/scalarizr/handlers/nginx.py
@@ -8,8 +8,9 @@ from scalarizr.bus import bus
 from scalarizr.handlers import Handler
 from scalarizr.behaviour import Behaviours
 from scalarizr.messaging import Messages
-from scalarizr.util import configtool
+from scalarizr.util import configtool, disttool
 import os
+import re
 import shutil
 import subprocess
 import logging
@@ -35,19 +36,40 @@ class NginxHandler(Handler):
 		config = bus.config
 		section = configtool.get_behaviour_section_name(Behaviours.WWW)
 		nginx_binary = config.get(section, "binary_path")
-		include = config.get(section, "app_include_path")
 		app_port = config.get(section, "app_port") or "80"
+		include = config.get(section, "app_include_path")
+		config_dir = os.path.dirname(include)
+		nginx_conf_path = config_dir + '/nginx.conf'
+		default_conf_path = config_dir + '/sites-enabled/' + 'default'
 		
 		template_path = os.path.join(bus.etc_path, "public.d/handler.nginx/app-servers.tpl")
+		
 		if not os.path.exists(template_path):
 			self._logger.warning("nginx template '%s' doesn't exists. Creating default template", template_path)
 			template_dir = os.path.dirname(template_path)
 			if not os.path.exists(template_dir):
 				os.makedirs(template_dir)
-			f = open(template_path, "w+")
-			f.write("""\nupstream backend {\n\tip_hash;\n\n\t${upstream_hosts}\n}\n""")
-			f.close()
-		template = open(template_path, 'r').read()
+			
+			file = None
+			try:
+				file = open(template_path, "w+")
+				file.write("""\nupstream backend {\n\tip_hash;\n\n\t${upstream_hosts}\n}\n""")
+			except IOError, e:
+				self._logger.error("Cannot write to %s file: %s" % (template_path, str(e)))
+			finally:
+				if file:
+					file.close()
+		
+		template = ""
+		file = None
+		try:			
+			file = open(template_path, 'r')
+			template = file.read()
+		except IOError, e:
+			self._logger.error("Cannot read %s file: %s" % (template_path, str(e)))
+		finally:
+			if file:
+				file.close()	
 
 		# Create upstream hosts configuration
 		upstream_hosts = ""
@@ -94,10 +116,88 @@ class NginxHandler(Handler):
 			if os.path.isfile(include):
 				shutil.move(include, include+".save")
 			self._logger.debug("Writing template to %s", include)
-			file = open(include, "w")
-			file.write(template)
-			file.close()
 			
+			file = None
+			try:
+				file = open(include, "w")
+				file.write(template)
+			except IOError, e:
+				self._logger.error("Cannot write to %s file: %s" % (include, str(e)))
+			finally:
+				if file:
+					file.close()
+			
+			nginx_conf = None
+			file = None
+			if not os.path.isfile(nginx_conf_path):
+				self._logger.error("nginx main config file % does not exist", nginx_conf_path)
+			else:
+				self._logger.debug("Trying to read nginx main config %s", nginx_conf_path)
+				
+				try:
+					file = open(nginx_conf_path,'r')
+					nginx_conf = file.read()
+				except IOError, e:
+					self._logger.error("Cannot read %s file: %s" % (nginx_conf_path, str(e)))
+				finally:
+					if file:
+						file.close()
+												
+				if nginx_conf:
+					include_regexp = re.compile('^[^#\n]*?include\s*'+include+'\s*;\s*$', re.MULTILINE)
+					if not re.search(include_regexp, nginx_conf):
+						new_nginx_conf = re.sub(re.compile('(http\s*\{.*?)(\})', re.S),
+								'\\1\n' + '    include '+ include + ';\n' + '\\2', nginx_conf)
+						self._logger.debug("Including generated config to main nginx config %s", nginx_conf_path)
+						
+						try:
+							file = open(nginx_conf_path,'w')
+							file.write(new_nginx_conf)
+						except IOError, e:
+							self._logger.error("Cannot write to %s file: %s" % (nginx_conf_path, str(e)))
+						finally:
+							if file:
+								file.close()
+					
+					else:
+						self._logger.debug("File %s already included into nginx main config %s", 
+									include, nginx_conf_path)
+						
+			if disttool._is_debian_based and os.path.isfile(default_conf_path):		
+				self._logger.debug("Patching nginx default vhost file (for debian-based dists only)")
+				
+				default_content = None
+				file = None
+				try:
+					file = open(default_conf_path)
+					default_content = file.read()
+				except IOError, e:
+					self._logger.error("Cannot read %s file: %s" % (default_conf_path, str(e)))
+				finally:
+					if file:
+						file.close()
+						
+				root_locat = re.compile('(?P<loc>^\s*location\s*/\s*\{[^\}]*?)(?P<root>^\s*root.*?;.*?$)(?P<endloc>[^\}]*?\})', re.DOTALL | re.MULTILINE)
+				
+				if default_content:
+					if re.search(root_locat, default_content):
+						new_content = re.sub(root_locat, '\\loc'+' '*16 +'proxy_pass http://backend;\\endloc', default_content)
+						
+						file = None 
+						try:
+							file = open(default_conf_path, 'w')
+							file.write(new_content)
+						except IOError, e:
+							self._logger.error("Cannot write to %s file: %s" % (default_conf_path, str(e)))
+						finally:
+							if file:
+								file.close()						
+					
+					else:
+						location_re = re.compile('(?P<loc>^\s*location\s*/\s*\{\s*$)(?P<endloc>[^\}]*\})')
+					
+											
+			#FIXME: use initd for starting, stopping & reloading nginx
 			self._logger.info("Testing new configuration.")
 			nginx_pid_file = "/var/run/nginx.pid"
 			nginx_test_command = [nginx_binary, "-t"]
@@ -116,10 +216,22 @@ class NginxHandler(Handler):
 			
 			elif os.path.isfile(nginx_binary) and os.path.isfile(nginx_pid_file):
 				self._logger.info("Reloading nginx.")
-				nginx_pid = open(nginx_pid_file,'r').read()
+				
+				nginx_pid = None
+				file = None
+				try:
+					file =  open(nginx_pid_file,'r')
+					nginx_pid = file.read()
+				except IOError, e:
+					self._logger.error("Cannot read nginx pid file %s: %s" % (nginx_pid_file, str(e)))
+				finally:
+					if file:
+						file.close()
+						
 				if nginx_pid.endswith('\n'):
 					nginx_pid = nginx_pid[:-1]
-				if "" != nginx_pid :
+					
+				if nginx_pid :
 					nginx_restart_command = ["kill","-HUP", nginx_pid]
 					subprocess.call(nginx_restart_command)
 				else:
