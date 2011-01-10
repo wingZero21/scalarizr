5f6e9add13ec1bc7d5e6f0acb2ca8309528e807e
Dmitry Korsakov <dmitry@scalr.com>
2011-01-10 09:37:53 +0000

    Fixed rsync attributes error (-x-x)

--- src/scalarizr/handlers/ec2/rebundle.py
+++ src/scalarizr/handlers/ec2/rebundle.py
@@ -878,7 +878,8 @@ if disttool.is_linux():
 			self._logger.info("Copying %s into the image %s", source, dest)
 			rsync = filetool.Rsync()
 			#rsync.archive().times().sparse().links().quietly()
-			rsync.archive().sparse().xattributes()
+			#rsync.archive().sparse().xattributes()
+			rsync.archive().sparse()
 			if xattr:
 				rsync.xattributes()
 			rsync.exclude(self.excludes)
