aec40252993a08bafed592ed42c32ba07d1cfa73
Marat Komarov <marat@scalr.com>
2011-01-31 18:13:19 +0000

    Released 0.7.0

--- externals/pysnmp/smi/exval.py
+++
@@ -1,6 +0,0 @@
-from pyasn1.type import univ, tag
-
-noSuchObject = univ.Null('').subtype(implicitTag=tag.Tag(tag.tagClassContext, tag.tagFormatSimple, 0x00))
-noSuchInstance = univ.Null('').subtype(implicitTag=tag.Tag(tag.tagClassContext, tag.tagFormatSimple, 0x01))
-endOfMib = univ.Null('').subtype(implicitTag=tag.Tag(tag.tagClassContext, tag.tagFormatSimple, 0x02))
-
--- test/misc/lvm2_freeze_test/run_sysbench.sh
+++
@@ -1,2 +0,0 @@
-cd /mnt/first/ && sysbench --max-requests=100000 --test=fileio --file-total-size=1900M --file-test-mode=rndrw run
-
