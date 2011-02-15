54d935dfc8f4fa3ed66db844288985ea4e8a4a92
Dmitry Korsakov <dmitry@scalr.com>
2011-02-15 14:06:05 +0000

    added _request and delete methods

--- src/scalarizr/libs/nimbula/__init__.py
+++ src/scalarizr/libs/nimbula/__init__.py
@@ -9,25 +9,48 @@ import httplib
 import urllib
 import urllib2
 import json
+import string
+import random
+
 
 nimbula_url = 'NIMBULA_URL'
 nimbula_username = 'NIMBULA_USERNAME'
 nimbula_password = 'NIMBULA_PASSWORD'
 
 def authenticate():
+	'''
+	['Server: nginx/0.6.32\r\n', 'Date: Tue, 15 Feb 2011 07:37:29 GMT\r\n', 
+	'Content-Type: application/json\r\n', 'Connection: close\r\n', 
+	'Expires: Tue, 15 Feb 2011 07:37:29 GMT\r\n', 
+	'Set-Cookie: nimbula={"realm": "beach", "value": 
+	"{\\"customer\\": \\"scalr\\", \\"expires\\": 1297759049.3628099, 
+	\\"realm\\": \\"beach\\", \\"user\\": \\"/scalr/administrator\\", 
+	\\"entity_type\\": \\"user\\"}", "signature": 
+	"AFZNnE0e0SVkEYFIFM5jnDdfvKztL3g8YaBvkm9HHPh+Q/9aDNPR0VpP9iqs530wl
+	fRKIGWXXJVQnL9xlxJQqsbUkxzAbarGzvwfg5+Zf3/BozyjwhhaYSljlAuHTBVueBz
+	NTi8FyLhHIOWii9T0rJV+zOKaHPRr02D92sdAtt2cS8Xdb31Ax6e5UcQMij3WD0gIz
+	TxZVawaI6veQZXrsarDXGBr1rQZzg+s8KH2A+O3D56DE+1jBvZEMN6+/BLO7m9OZsf
+	s7XqPkc5N5UEYMSZVrI3rUDNlHf2j9Um99wibUdL5oDhQ2n0SOmzm1bC1HEbEGaDT5
+	Jj0eabxTEhi3Gm9kKck"}; Expires=Tue, 15-Feb-2011 08:37:29 GMT; Path=/\r\n']
+	'''
+	
 	login = os.environ[nimbula_username]
 	pwd = os.environ[nimbula_password]
+	auth_basename = '/authenticate/'
 	
 	if not (login or pwd):
 		raise NimbulaError('No login inforation found')
 	
+	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+	uri = urlparse.urljoin(os.environ[nimbula_url], auth_basename)
+	request = urllib2.Request(uri)
+	
+	request.add_header('Accept', 'application/json')
+	request.add_header('Content-Type', 'application/json')
+	
 	raw_data = dict(user=login, password=pwd)
 	data = json.dumps(raw_data)
 	
-	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-	request = urllib2.Request('HTTPS://serverbeach.demo.nimbula.com:443/authenticate/')
-	request.add_header('Accept', 'application/json')
-	request.add_header('Content-Type', 'application/json')
 	urllib2.install_opener(opener)
 	response = opener.open(request, data=data)
 	
@@ -38,16 +61,8 @@ def authenticate():
 	
 	info = response.info()
 	headers = info.headers
-	
 	return headers
-		
-	'''
-	headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
-	conn = httplib.HTTPSConnection(os.environ[nimbula_url])
-	conn.request("POST", '/authenticate/', data, headers)
-	response = conn.getresponse()
-	reg = response.getheaders()
-	'''
+
 
 class NimbulaConnection:
 	username = None
@@ -59,11 +74,34 @@ class NimbulaConnection:
 		self.username = username
 		self.password = password
 		
-	def _get_image_URI(self, name):
-		basename = 'machineimage'
-		if not name.startswith('/'):
-			name = '/scalr/%s/%s' % (self.username, name)
-		return urlparse.urljoin(self.api_url,basename+name)
+	def _get_object_URI(self, objname, basename=None):
+		basename = basename or 'machineimage'
+		if not objname.startswith('/'):
+			objname = '%s/%s' % (self.username, objname)
+		return urlparse.urljoin(self.api_url,basename+objname)
+	
+
+	def _request(self, uri, headers=None, query_method=None, force=True):
+		request = urllib2.Request(uri)
+		
+		headers = headers or [('Accept', 'application/json'), ('Content-Type', 'application/json')]
+		for header in headers:
+			request.add_header(*header)
+			
+		if query_method:
+			request.get_method = lambda: query_method
+			
+		try:
+			f = urllib2.urlopen(request)
+		except urllib2.HTTPError, e:
+			if e.code==401 and force==True:
+				authenticate()
+				return self._request(uri, headers, query_method, force=False)
+			else:
+				raise NimbulaError(e)
+			
+		return f
+		
 		
 	def add_machine_image(self, name, file=None, fp=None, attributes=None, account=None):
 		''' 
@@ -74,30 +112,29 @@ class NimbulaConnection:
 		if not file and not fp:
 			raise NimbulaError
 		
-		uri = self._get_image_URI(name)
+		boundary = "".join([random.choice(string.ascii_lowercase+string.digits) for x in xrange(31)])
 		
+		uri = self._get_object_URI(name)
+	
+	
 	def get_machine_image(self, name, force=True):
 		'''
 		 '{"attributes": {"nimbula_compressed_size": 97120551, "nimbula_decompressed_size": 5905612288}, 
 		 "account": null, "uri": "https://serverbeach.demo.nimbula.com:443/machineimage/nimbula/public/default", 
 		 "file": null, "name": "/nimbula/public/default"}'
 		'''
-		uri = self._get_image_URI(name)
-		try:
-			f = urllib2.urlopen(uri)
-		except urllib2.HTTPError, e:
-			if e.code==401 and force:
-				authenticate()
-				return self.get_machine_image(name, force=False)
-				
-				
+		uri = self._get_object_URI(name)
+		f = self._request(uri)
 		response = f.read()
 		return response
 
+	
+	def delete_machine_image(self, name, force=True):
+		uri = self._get_object_URI(name)
+		f = self._request(uri, query_method='DELETE')
+		response = f.read()
+		return response
 		
-		
-	def delete_machine_image(self, name):
-		uri = self._get_URI(name)
 		
 	def discover_machine_image(self, container=None):
 		pass
--- test/unit/testcases/szr_unittest/libs_test/nimbula_test/test.py
+++ test/unit/testcases/szr_unittest/libs_test/nimbula_test/test.py
@@ -23,35 +23,37 @@ class NimbulaConnectionTest(unittest.TestCase):
 
 	def setUp(self):
 		self.url = os.environ[nimbula_url]
-		self.login = 'test'
+		self.login = os.environ[nimbula_username]
 		self.conn = NimbulaConnection(self.url,self.login)
 		
-	def _test_authenticate(self):
+	def test_authenticate(self):
 		cookie = authenticate()
-		print "GOT COOKIES:", cookie
+		self.assertTrue(self.login in str(cookie))
+		#print "GOT COOKIES:", cookie
 
 	def tearDown(self):
 		pass
 	
 	def test_get_URI(self):
-		self.assertEquals(self.conn._get_image_URI('/name'), self.url+'/machineimage/name')
-		self.assertEquals(self.conn._get_image_URI('name2'), '%s/machineimage/scalr/%s/name2'%(self.url,self.login))
-		pass
+		self.assertEquals(self.conn._get_object_URI('/scalr/administrator/imagename'), self.url+'/machineimage/scalr/administrator/imagename')
+		self.assertEquals(self.conn._get_object_URI('imagename2'), '%s/machineimage%s/imagename2'%(self.url,self.login))
 
 	def test_get_machine_image(self):
 		image_name = '/nimbula/public/default'
 		info = self.conn.get_machine_image(image_name)
-		print "GOT SERVER INFO:", info
+		#print "GOT SERVER INFO:", info
 		self.assertTrue(image_name in str(info))
 		
 		info2 = self.conn.get_machine_image(image_name)
-		print "GOT SERVER INFO2:", info2
+		#print "GOT SERVER INFO2:", info2
 		
 	def test_add_machine_image(self):
 		pass
 
 	def test_delete_machine_image(self):
-		pass
+		image_name = '/nimbula/public/default'
+		info = self.conn.delete_machine_image(image_name)
+		print info
 
 	def test_discover_machine_image(self):
 		pass
