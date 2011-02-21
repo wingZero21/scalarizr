8271e77134783b5b722f4229504b6ab1fb6c4804
Marat Komarov <marat@scalr.com>
2011-02-21 15:05:32 +0000

    * Fixed: don't call platform.get_user_data in _detect_scalr_version during import

--- src/scalarizr/__init__.py
+++ src/scalarizr/__init__.py
@@ -308,7 +308,8 @@ def _apply_user_data(cnf):
 
 def _detect_scalr_version():
 	pl = bus.platform
-	if pl:
+	cnf = bus.cnf
+	if pl and cnf.state != ScalarizrState.IMPORTING:
 		return (2, 2) if pl.get_user_data('cloud_storage_path') else (2, 1)
 	return (2, 0)	
 
