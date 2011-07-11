de6a3ee5b25b8e6107b294ca9568ce4163be529c
Marat Komarov <marat@scalr.com>
2011-07-11 16:25:54 +0000

    test commit

--- src/scalarizr/__init__.py
+++ src/scalarizr/__init__.py
@@ -35,7 +35,7 @@ class NotConfiguredError(BaseException):
 	pass
 
 
-__version__ = open(os.path.join(os.path.dirname(__file__), 'version')).read()
+__version__ = open(os.path.join(os.path.dirname(__file__), 'version')).read().strip()
 
 
 EMBED_SNMPD = True
