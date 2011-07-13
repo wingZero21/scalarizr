e90ce866518e756699637a59406bdd9ffa86e9ae
Marat Komarov <marat@scalr.com>
2011-07-13 16:34:40 +0000

    test

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from distutils.util import change_root
 from distutils.command.install_data import install_data
 import platform
 
+
 class my_install_data(install_data):
 	def run(self):
 		install_data.run(self)
