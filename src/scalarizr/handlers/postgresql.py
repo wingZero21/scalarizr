d95ada1cd7e8c52fde1e4dfcb873f9613bdd2710
Dmitry Korsakov <dmitry@scalr.com>
2011-07-21 13:10:02 +0000

    Fix for replication_master option

--- src/scalarizr/handlers/postgresql.py
+++ src/scalarizr/handlers/postgresql.py
@@ -227,7 +227,7 @@ class PostgreSqlHander(ServiceCtlHanler):
 		if not message.body.has_key(BEHAVIOUR) or message.db_type != BEHAVIOUR:
 			raise HandlerError("HostInitResponse message for PostgreSQL behaviour must have 'postgresql' property and db_type 'postgresql'")
 		
-		if message.postgresql.replication_master != '1'  and \
+		if message.postgresql[OPT_REPLICATION_MASTER] != '1'  and \
 				(not message.body.has_key(OPT_ROOT_SSH_PUBLIC_KEY) or not 
 				message.body.has_key(OPT_ROOT_SSH_PRIVATE_KEY)):
 			raise HandlerError("HostInitResponse message for PostgreSQL behaviour must contain both public and private ssh keys")
