9571fa24cd9f26146d10caaa695529cfe7d6f2ef
Dmitry Korsakov <dmitry@scalr.com>
2011-07-27 06:54:56 +0000

    Scalarizr must check and start postgresql service after reboot / restart

--- src/scalarizr/handlers/postgresql.py
+++ src/scalarizr/handlers/postgresql.py
@@ -195,6 +195,8 @@ class PostgreSqlHander(ServiceCtlHanler):
 					except ValueError:
 						self._logger.warning("Scalr's root PgSQL user was changed. Recreating.")
 						self.postgresql.root_user.change_password(root_password)
+		else:
+			self.postgresql.service.start()
 
 
 	def on_reload(self):
