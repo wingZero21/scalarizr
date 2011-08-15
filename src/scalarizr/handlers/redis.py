559787fd72a370830bdd61dd349ad95554f67efd
Dmitry Korsakov <dmitry@scalr.com>
2011-08-15 08:07:31 +0000

    Experimental redis support, should work on Ubuntu/EC2

---
+++ src/scalarizr/handlers/redis.py
@@ -0,0 +1,495 @@
+'''
+Created on Aug 12, 2011
+
+@author: shaitanich
+'''
+
+import os
+import time
+import shutil
+import tarfile
+import tempfile
+import logging
+
+from scalarizr import config
+from scalarizr.bus import bus
+from scalarizr.messaging import Messages
+from scalarizr.util import system2, wait_until
+from scalarizr.util.filetool import split, rchown
+from scalarizr.services.redis import Redis, REDIS_DEFAULT_PORT, REDIS_DB_FILENAME
+from scalarizr.config import BuiltinBehaviours, ScalarizrState
+from scalarizr.handlers import ServiceCtlHanler, HandlerError, DbMsrMessages
+from scalarizr.storage import Storage, Snapshot, StorageError, Volume, transfer
+
+
+BEHAVIOUR = SERVICE_NAME = CNF_SECTION = BuiltinBehaviours.REDIS
+
+STORAGE_PATH 				= '/mnt/redis'
+STORAGE_VOLUME_CNF 			= 'redis.json'
+STORAGE_SNAPSHOT_CNF 		= 'redis-snap.json'
+
+OPT_REPLICATION_MASTER  	= 'replication_master'
+OPT_MASTER_PASSWORD			= "master_password"
+OPT_VOLUME_CNF				= 'volume_config'
+OPT_SNAPSHOT_CNF			= 'snapshot_config'
+
+REDIS_USER 					= 'redis'
+BACKUP_CHUNK_SIZE 			= 200*1024*1024
+
+
+def get_handlers():
+	return (RedisHander(), )
+
+
+class RedisHander(ServiceCtlHanler):	
+	_logger = None
+		
+	_queryenv = None
+	""" @type _queryenv: scalarizr.queryenv.QueryEnvService	"""
+	
+	_platform = None
+	""" @type _platform: scalarizr.platform.Ec2Platform """
+	
+	_cnf = None
+	''' @type _cnf: scalarizr.config.ScalarizrCnf '''
+	
+	storage_vol = None	
+		
+	@property
+	def is_replication_master(self):
+		value = self._cnf.rawini.get(CNF_SECTION, OPT_REPLICATION_MASTER)
+		self._logger.debug('Got %s : %s' % (OPT_REPLICATION_MASTER, value))
+		return True if int(value) else False
+			
+	def accept(self, message, queue, behaviour=None, platform=None, os=None, dist=None):
+		return BEHAVIOUR in behaviour and (
+					message.name == DbMsrMessages.DBMSR_NEW_MASTER_UP
+				or 	message.name == DbMsrMessages.DBMSR_PROMOTE_TO_MASTER
+				or 	message.name == DbMsrMessages.DBMSR_CREATE_DATA_BUNDLE
+				or 	message.name == DbMsrMessages.DBMSR_CREATE_BACKUP
+				or  message.name == Messages.UPDATE_SERVICE_CONFIGURATION
+				or  message.name == Messages.HOST_INIT
+				or  message.name == Messages.BEFORE_HOST_TERMINATE)	
+
+	
+	def __init__(self):
+		self._logger = logging.getLogger(__name__)
+		bus.on("init", self.on_init)
+		bus.define_events(
+			'before_%s_data_bundle' % BEHAVIOUR,
+			
+			'%s_data_bundle' % BEHAVIOUR,
+			
+			# @param host: New master hostname 
+			'before_%s_change_master' % BEHAVIOUR,
+			
+			# @param host: New master hostname 
+			'%s_change_master' % BEHAVIOUR,
+			
+			'before_slave_promote_to_master',
+			
+			'slave_promote_to_master'
+		)	
+		
+		self.on_reload()		
+
+	
+	def on_init(self):		
+			
+		bus.on("host_init_response", self.on_host_init_response)
+		bus.on("before_host_up", self.on_before_host_up)
+		bus.on("before_reboot_start", self.on_before_reboot_start)
+		bus.on("before_reboot_finish", self.on_before_reboot_finish)
+		
+		if self._cnf.state == ScalarizrState.RUNNING:
+
+			storage_conf = Storage.restore_config(self._volume_config_path)
+			self.storage_vol = Storage.create(storage_conf)
+			if not self.storage_vol.mounted():
+				self.storage_vol.mount()
+			
+			self.redis.service.start()
+			
+
+	def on_reload(self):
+		self._queryenv = bus.queryenv_service
+		self._platform = bus.platform
+		self._cnf = bus.cnf
+		ini = self._cnf.rawini
+		self._role_name = ini.get(config.SECT_GENERAL, config.OPT_ROLE_NAME)
+		
+		self._storage_path = STORAGE_PATH
+		
+		self._volume_config_path  = self._cnf.private_path(os.path.join('storage', STORAGE_VOLUME_CNF))
+		self._snapshot_config_path = self._cnf.private_path(os.path.join('storage', STORAGE_SNAPSHOT_CNF))
+		
+		self.redis = Redis(self.is_replication_master)
+		
+				
+
+	def on_host_init_response(self, message):
+		"""
+		Check redis data in host init response
+		@type message: scalarizr.messaging.Message
+		@param message: HostInitResponse
+		"""
+		if not message.body.has_key(BEHAVIOUR) or message.db_type != BEHAVIOUR:
+			raise HandlerError("HostInitResponse message for %s behaviour must have '%s' property and db_type '%s'" 
+							% (BEHAVIOUR, BEHAVIOUR, BEHAVIOUR))
+		
+		
+		dir = os.path.dirname(self._volume_config_path)
+		if not os.path.exists(dir):
+			os.makedirs(dir)
+		
+		redis_data = message.redis.copy()	
+		
+		for key, file in ((OPT_VOLUME_CNF, self._volume_config_path), 
+						(OPT_SNAPSHOT_CNF, self._snapshot_config_path)):
+			if os.path.exists(file):
+				os.remove(file)
+			
+			if key in redis_data:
+				if redis_data[key]:
+					Storage.backup_config(redis_data[key], file)
+				del redis_data[key]
+		
+		self._logger.debug("Update redis config with %s", redis_data)
+		self._update_config(redis_data)
+
+
+	def on_before_host_up(self, message):
+		"""
+		Configure redis behaviour
+		@type message: scalarizr.messaging.Message		
+		@param message: HostUp message
+		"""
+
+		repl = 'master' if self.redis.is_replication_master else 'slave'
+		
+		if self.redis.is_replication_master:
+			self._init_master(message)									  
+		else:
+			self._init_slave(message)		
+			
+		bus.fire('service_configured', service_name=SERVICE_NAME, replication=repl)
+					
+					
+	def on_before_reboot_start(self, *args, **kwargs):
+		self.redis.service.stop('rebooting')
+
+
+	def on_before_reboot_finish(self, *args, **kwargs):
+		#TODO: find out what to do!
+		pass
+
+
+	def on_BeforeHostTerminate(self, message):
+		if message.local_ip == self._platform.get_private_ip():
+			self.redis.service.stop('Server will be terminated')
+			self._logger.info('Detaching PgSQL storage')
+			self.storage_vol.detach()
+	
+			
+	
+	def on_DbMsr_CreateDataBundle(self, message):
+		
+		try:
+			bus.fire('before_%s_data_bundle' % BEHAVIOUR)
+			# Creating snapshot		
+			snap = self._create_snapshot(REDIS_USER, self.redis.password)
+			used_size = int(system2(('df', '-P', '--block-size=M', self._storage_path))[0].split('\n')[1].split()[2][:-1])
+			bus.fire('%s_data_bundle' % BEHAVIOUR, snapshot_id=snap.id)			
+			
+			# Notify scalr
+			msg_data = dict(
+				db_type 	= BEHAVIOUR,
+				used_size	= '%.3f' % (float(used_size) / 1000,),
+				status		= 'ok'
+			)
+			msg_data[BEHAVIOUR] = self._compat_storage_data(snap=snap)
+			self.send_message(DbMsrMessages.DBMSR_CREATE_DATA_BUNDLE_RESULT, msg_data)
+
+		except (Exception, BaseException), e:
+			self._logger.exception(e)
+			
+			# Notify Scalr about error
+			self.send_message(DbMsrMessages.DBMSR_CREATE_DATA_BUNDLE_RESULT, dict(
+				db_type 	= BEHAVIOUR,
+				status		='error',
+				last_error	= str(e)
+			))
+			
+	
+	def on_DbMsr_PromoteToMaster(self, message):
+		"""
+		Promote slave to master
+		@type message: scalarizr.messaging.Message
+		@param message: redis_PromoteToMaster
+		"""
+		
+		if message.db_type != BEHAVIOUR:
+			self._logger.error('Wrong db_type in DbMsr_PromoteToMaster message: %s' % message.db_type)
+			return
+		
+		if self.redis.is_replication_master:
+			self._logger.warning('Cannot promote to master. Already master')
+			return
+		bus.fire('before_slave_promote_to_master')
+		try:
+			msg_data = dict(
+					db_type=BEHAVIOUR, 
+					status="ok",
+			)
+			self.redis.init_master(self._storage_path)
+			self._update_config({OPT_REPLICATION_MASTER : "1"})	
+									
+			snap = self._create_snapshot(REDIS_USER, message.root_password)
+			Storage.backup_config(snap.config(), self._snapshot_config_path)
+			msg_data[BEHAVIOUR] = self._compat_storage_data(self.storage_vol.config(), snap)
+				
+			self.send_message(DbMsrMessages.DBMSR_PROMOTE_TO_MASTER_RESULT, msg_data)	
+								
+			bus.fire('slave_promote_to_master')
+			
+		except (Exception, BaseException), e:
+			self._logger.exception(e)
+			
+			self.send_message(DbMsrMessages.DBMSR_PROMOTE_TO_MASTER_RESULT, dict(
+				db_type=BEHAVIOUR, 															
+				status="error",
+				last_error=str(e)
+			))
+
+
+	def on_DbMsr_NewMasterUp(self, message):
+		"""
+		Switch replication to a new master server
+		@type message: scalarizr.messaging.Message
+		@param message:  DbMsr__NewMasterUp
+		"""
+		if not message.body.has_key(BEHAVIOUR) or message.db_type != BEHAVIOUR:
+			raise HandlerError("DbMsr_NewMasterUp message for %s behaviour must have '%s' property and db_type '%s'" % 
+							BEHAVIOUR, BEHAVIOUR, BEHAVIOUR)
+		
+		if self.redis.is_replication_master:
+			self._logger.debug('Skipping NewMasterUp. My replication role is master')	
+			return 
+
+		host = message.local_ip or message.remote_ip
+		self._logger.info("Switching replication to a new %s master %s"% (BEHAVIOUR, host))
+		bus.fire('before_%s_change_master' % BEHAVIOUR, host=host)			
+			
+		self.redis.init_slave(self._storage_path, host, REDIS_DEFAULT_PORT)
+			
+		self._logger.debug("Replication switched")
+		bus.fire('%s_change_master' % BEHAVIOUR, host=host)
+			
+	
+	def on_DbMsr_CreateBackup(self, message):
+		tmpdir = backup_path = None
+		try:
+			# Dump all databases
+			self._logger.info("Dumping all databases")			
+			tmpdir = tempfile.mkdtemp()		
+			src_path = os.path.join(self.redis.redis_conf.dir, REDIS_DB_FILENAME)
+			dump_path = os.path.join(tmpdir, REDIS_DB_FILENAME)
+			
+			if not os.path.exists(src_path):
+				raise BaseException('Redis DB file %s does not exists. Skipping Backup process')
+			
+			# Defining archive name and path
+			backup_filename = 'redis-backup-'+time.strftime('%Y-%m-%d-%H:%M:%S')+'.tar.gz'
+			backup_path = os.path.join('/tmp', backup_filename)
+
+			shutil.copyfile(src_path, dump_path)
+			rchown('redis', tmpdir)
+			
+			# Creating archive 
+			backup = tarfile.open(backup_path, 'w:gz')
+			backup.add(dump_path, REDIS_DB_FILENAME)
+			backup.close()
+			
+			# Creating list of full paths to archive chunks
+			if os.path.getsize(backup_path) > BACKUP_CHUNK_SIZE:
+				parts = [os.path.join(tmpdir, file) for file in split(backup_path, backup_filename, BACKUP_CHUNK_SIZE , tmpdir)]
+			else:
+				parts = [backup_path]
+					
+			self._logger.info("Uploading backup to cloud storage (%s)", self._platform.cloud_storage_path)
+			trn = transfer.Transfer()
+			result = trn.upload(parts, self._platform.cloud_storage_path)
+			self._logger.info("%s backup uploaded to cloud storage under %s/%s" % 
+						BEHAVIOUR, self._platform.cloud_storage_path, backup_filename)
+			
+			# Notify Scalr
+			self.send_message(DbMsrMessages.DBMSR_CREATE_BACKUP_RESULT, dict(
+				db_type = BEHAVIOUR,
+				status = 'ok',
+				backup_parts = result
+			))
+						
+		except (Exception, BaseException), e:
+			self._logger.exception(e)
+			
+			# Notify Scalr about error
+			self.send_message(DbMsrMessages.DBMSR_CREATE_BACKUP_RESULT, dict(
+				db_type = BEHAVIOUR,
+				status = 'error',
+				last_error = str(e)
+			))
+			
+		finally:
+			if tmpdir:
+				shutil.rmtree(tmpdir, ignore_errors=True)
+			if backup_path and os.path.exists(backup_path):
+				os.remove(backup_path)				
+		
+							
+	def _init_master(self, message):
+		"""
+		Initialize redis master
+		@type message: scalarizr.messaging.Message 
+		@param message: HostUp message
+		"""
+		
+		self._logger.info("Initializing %s master" % BEHAVIOUR)
+		
+		# Plug storage
+		volume_cnf = Storage.restore_config(self._volume_config_path)
+		try:
+			snap_cnf = Storage.restore_config(self._snapshot_config_path)
+			volume_cnf['snapshot'] = snap_cnf
+		except IOError:
+			pass
+		self.storage_vol = self._plug_storage(mpoint=self._storage_path, vol=volume_cnf)
+		Storage.backup_config(self.storage_vol.config(), self._volume_config_path)	
+			
+		password = self._get_password()
+		self.redis.init_master(mpoint=self._storage_path, password)
+		
+		msg_data = dict()
+		msg_data.update({OPT_REPLICATION_MASTER 		: 	'1',
+							OPT_MASTER_PASSWORD			:	password})	
+			
+		# Create snapshot
+		snap = self._create_snapshot(REDIS_USER, password)
+		Storage.backup_config(snap.config(), self._snapshot_config_path)
+	
+		# Update HostUp message 
+		msg_data.update(self._compat_storage_data(self.storage_vol, snap))
+			
+		if msg_data:
+			message.db_type = BEHAVIOUR
+			message.redis = msg_data.copy()
+			try:
+				del msg_data[OPT_SNAPSHOT_CNF], msg_data[OPT_VOLUME_CNF]
+			except KeyError:
+				pass 
+			self._update_config(msg_data)		
+
+	def _get_password(self):
+		password = None 
+		opt_pwd = '%s_password' % BEHAVIOUR
+		if self._cnf.rawini.has_option(CNF_SECTION, opt_pwd):
+			password = self._cnf.rawini.get(CNF_SECTION, opt_pwd)	
+		return password		
+	
+	def _get_master_host(self):
+		master_host = None
+		self._logger.info("Requesting master server")
+		while not master_host:
+			try:
+				master_host = list(host 
+					for host in self._queryenv.list_roles(self._role_name)[0].hosts 
+					if host.replication_master)[0]
+			except IndexError:
+				self._logger.debug("QueryEnv respond with no %s master. " % BEHAVIOUR + 
+						"Waiting %d seconds before the next attempt" % 5)
+				time.sleep(5)
+		return master_host
+
+				
+	def _init_slave(self, message):
+		"""
+		Initialize redis slave
+		@type message: scalarizr.messaging.Message 
+		@param message: HostUp message
+		"""
+		self._logger.info("Initializing %s slave" % BEHAVIOUR)
+			
+		# Change replication master 
+		master_host = self._get_master_host()
+				
+		self._logger.debug("Master server obtained (local_ip: %s, public_ip: %s)",
+				master_host.internal_ip, master_host.external_ip)
+		
+		host = master_host.internal_ip or master_host.external_ip
+		self.redis.init_slave(self._storage_path, host, REDIS_DEFAULT_PORT, self._get_password())
+		
+		# Update HostUp message
+		message.redis = self._compat_storage_data(self.storage_vol)
+		message.db_type = BEHAVIOUR
+
+
+	def _update_config(self, data): 
+		#XXX: I just don't like it
+		#ditching empty data
+		updates = dict()
+		for k,v in data.items():
+			if v: 
+				updates[k] = v
+		
+		self._cnf.update_ini(BEHAVIOUR, {CNF_SECTION: updates})
+
+
+	def _plug_storage(self, mpoint, vol):
+		if not isinstance(vol, Volume):
+			vol = Storage.create(vol)
+
+		try:
+			if not os.path.exists(mpoint):
+				os.makedirs(mpoint)
+			if not vol.mounted():
+				vol.mount(mpoint)
+		except StorageError, e:
+			if 'you must specify the filesystem type' in str(e):
+				vol.mkfs()
+				vol.mount(mpoint)
+			else:
+				raise
+		return vol
+
+
+	def _create_snapshot(self, root_user, root_password, dry_run=False):
+		
+		system2('sync', shell=True)
+		# Creating storage snapshot
+		snap = None if dry_run else self._create_storage_snapshot()
+			
+		wait_until(lambda: snap.state in (Snapshot.CREATED, Snapshot.COMPLETED, Snapshot.FAILED))
+		if snap.state == Snapshot.FAILED:
+			raise HandlerError('%s storage snapshot creation failed. See log for more details' % BEHAVIOUR)
+		
+		return snap
+
+
+	def _create_storage_snapshot(self):
+		self._logger.info("Creating storage snapshot")
+		try:
+			return self.storage_vol.snapshot()
+		except StorageError, e:
+			self._logger.error("Cannot create %s data snapshot. %s", (BEHAVIOUR, e))
+			raise
+		
+
+	def _compat_storage_data(self, vol=None, snap=None):
+		ret = dict()
+		if vol:
+			ret['volume_config'] = vol.config()
+		if snap:
+			ret['snapshot_config'] = snap.config()
+		return ret
+
+	
+	
---
+++ src/scalarizr/services/redis.py
@@ -0,0 +1,243 @@
+'''
+Created on Aug 10, 2011
+
+@author: shaitanich
+'''
+
+import os
+import logging
+import shutil
+
+from scalarizr.bus import bus
+from scalarizr.util import initdv2, system2, PopenError, wait_until
+from scalarizr.services import lazy, BaseConfig, BaseService
+from scalarizr.util import disttool, cryptotool
+from scalarizr.util.filetool import rchown
+from scalarizr.libs.metaconf import Configuration
+
+
+SERVICE_NAME = CNF_SECTION = 'redis'
+BIN_PATH 	 = '/usr/bin/redis-server'	
+OPT_REPLICATION_MASTER  = "replication_master"
+CONFIG_PATH = '/etc/redis/redis.conf'
+				
+REDIS_DEFAULT_PORT	= 6379	
+REDIS_DB_FILENAME = 'dump.rdb'			
+			
+				
+class RedisInitScript(initdv2.ParametrizedInitScript):
+	socket_file = None
+	
+	@lazy
+	def __new__(cls, *args, **kws):
+		obj = super(RedisInitScript, cls).__new__(cls, *args, **kws)
+		cls.__init__(obj)
+		return obj
+			
+	def __init__(self):
+		initdv2.ParametrizedInitScript.__init__(self, name=SERVICE_NAME, 
+				initd_script='/etc/init.d/redis-server')
+	
+	@property
+	def _processes(self):
+		args = ('ps', '-G', BIN_PATH, '-o', 'command', '--no-headers')	
+		try:	
+			p = [x for x in system2(args)[0].split('\n') if x and BIN_PATH + ' ' + CONFIG_PATH in x]
+		except PopenError,e:
+			p = []
+		return p
+	
+	def status(self):
+		return initdv2.Status.RUNNING if BIN_PATH in self._processes else initdv2.Status.NOT_RUNNING
+
+	def stop(self, reason=None):
+		initdv2.ParametrizedInitScript.stop(self)
+		wait_until(lambda: not self._processes, timeout=10, sleep=1)
+	
+	def restart(self, reason=None):
+		self.stop()
+		self.start()
+	
+	def reload(self, reason=None):
+		initdv2.ParametrizedInitScript.restart(self)
+		
+	def start(self):
+		initdv2.ParametrizedInitScript.start(self)
+	
+	
+initdv2.explore(SERVICE_NAME, RedisInitScript)
+	
+	
+class Redis(BaseService):
+	_instance = None
+	service = None
+
+	def __init__(self, master=False):
+		self._objects = {}
+		self.service = initdv2.lookup(SERVICE_NAME)
+		self._logger = logging.getLogger(__name__)
+		self._cnf = bus.cnf
+		self.is_replication_master = master
+						
+	def __new__(cls, *args, **kwargs):
+		if not cls._instance:
+			cls._instance = super(Redis, cls).__new__(
+								cls, *args, **kwargs)
+		return cls._instance				
+
+	def init_master(self, mpoint, password=None):
+		self.init_service(mpoint)
+		self.redis_conf.requirepass = password or self.generate_password()
+		self.service.start()
+		self.is_replication_master = True
+		
+	def init_slave(self, mpoint, primary_ip, primary_port, password):
+		self.init_service(mpoint)
+		self.change_primary(primary_ip, primary_port, password)
+		self.service.start()
+		self.is_replication_master = False
+		
+	def change_primary(self, primary_ip, primary_port, password):
+		self.working_directory.empty()
+		self.redis_conf.masterauth = password
+		self.redis_conf.slaveof = (primary_ip, primary_port)
+		
+	def init_service(self, mpoint):
+		move_files = not self.working_directory.is_initialized(mpoint)
+		self.working_directory.move_to(mpoint, move_files)
+		self.redis_conf.bind = None
+		self.redis_conf.dir = mpoint
+		
+	@property	
+	def password(self):
+		return self.redis_conf.requirepass if self.is_replication_master else self.redis_conf.masterauth
+	
+	def generate_password(self, length=20):
+		return cryptotool.pwgen(length)	
+	
+	def _get_redis_conf(self):
+		return self._get('redis_conf', RedisConf.find, self.config_dir)
+	
+	def _set_redis_conf(self, obj):
+		self._set('redis_conf', obj)
+	
+	def _get_working_directory(self):
+		return self._get('working_directory', WorkingDirectory.find, self.redis_conf)
+		
+	def _set_working_directory(self, obj):
+		self._set('working_directory', obj)
+		
+	working_directory = property(_get_working_directory, _set_working_directory)
+	redis_conf = property(_get_redis_conf, _set_redis_conf)
+	
+	
+class WorkingDirectory(object):
+	
+	default_centos_path = default_ubuntu_path = '/var/lib/redis'
+	
+	def __init__(self, path=None, user = "redis"):
+		self.path = path
+		self.user = user
+		self._logger = logging.getLogger(__name__)
+		
+	@classmethod
+	def find(cls, redis_conf):
+		path = redis_conf.dir
+		if not path:
+			path = cls.default_ubuntu_path if disttool.is_ubuntu() else cls.default_centos_path
+		return cls(path)
+
+	def move_to(self, dst, move_files=True):
+		if not os.path.exists(dst):
+			self._logger.debug('Creating directory structure for redis db files: %s' % dst)
+			os.makedirs(dst)
+		
+		if move_files and os.path.exists(self.path) and os.listdir(self.path):
+			self._logger.debug("copying db files from %s into %s" % (self.path, dst))
+			shutil.copytree(self.path, dst)	
+		self._logger.debug("changing directory owner to %s" % self.user)	
+		rchown(self.user, dst)			
+		self.path = dst
+		return dst
+
+	def is_initialized(self, path):
+		# are the redis db files already in place? 
+		return os.path.exists(path) and REDIS_DB_FILENAME in os.listdir(path)
+	
+	def empty(self):
+		self._logger.info('Emptying redis database dir %s' % self.path)
+		try:
+			for fname in os.listdir(self.path):
+				if fname.endswith('.rdb'):
+					path = os.path.join(self.path, fname)
+					if os.path.isfile(path):
+						self._logger.debug('Deleting redis db file %s' % path)
+						os.remove(path)
+					elif os.path.islink(path):
+						self._logger.debug('Deleting link to redis db file %s' % path)
+						os.unlink(path)						
+		except OSError, e:
+			self._logger.error('Cannot empty %s: %s' % (self.path, e))
+				
+	
+	
+class BaseRedisConfig(BaseConfig):
+	config_type = 'redis'
+
+
+class RedisConf(BaseRedisConfig):
+	
+	config_name = 'redis.conf'
+
+	def set(self, option, value):
+		if not self.data:
+			self.data = Configuration(self.config_type)
+			if os.path.exists(self.path):
+				self.data.read(self.path)
+		if value:
+			self.data.set(option,value, force=True)
+		else: 
+			self.data.comment(option)
+		if self.autosave:
+			self.save()
+			self.data = None
+				
+	def _get_dir(self):
+		return self.get('dir')
+	
+	def _set_dir(self, path):
+		self.set_path_type_option('dir', path)
+	
+	def _get_bind(self):
+		return self.get('bind').split()
+	
+	def _set_bind(self, list_ips):
+		self.set_path_type_option('bind', ' '.join(list_ips))
+				
+	def _get_slaveof(self):
+		return self.get('slaveof').split()
+	
+	def _set_slaveof(self, conn_data):
+		'''
+		@tuple conndata: (ip,) or (ip, port)
+		'''
+		self.set_path_type_option('slaveof', ' '.join(conn_data))		
+	
+	def _get_masterauth(self):
+		return self.get('masterauth')
+	
+	def _set_masterauth(self, passwd):
+		self.set_path_type_option('masterauth', passwd)		
+	
+	def _get_requirepass(self):
+		return self.get('requirepass')
+	
+	def _set_requirepass(self, passwd):
+		self.set_path_type_option('requirepass', passwd)	
+				
+	dir = property(_get_dir, _set_dir)
+	bind = property(_get_bind, _set_bind)
+	slaveof = property(_get_slaveof, _set_slaveof)
+	masterauth = property(_get_masterauth, _set_masterauth)
+	requirepass	 = property(_get_requirepass, _set_requirepass)
+		
