c8d1920e1b9a9626282f59f199886d91a17514b2
Dmitry Korsakov <dmitry@scalr.com>
2011-08-15 14:01:47 +0000

    Added start_backup and stop_backup wrappers to psql

--- src/scalarizr/services/postgresql.py
+++ src/scalarizr/services/postgresql.py
@@ -575,7 +575,15 @@ class PSQL(object):
 	def delete_pg_database(self, name):
 		out = self.psql.execute('DROP DATABASE IF EXISTS %s;' % name)
 		self._logger.debug(out)
-			
+		
+	def start_backup(self):
+		out = self.psql.execute("SELECT pg_start_backup('label', true)")
+		self._logger.debug(out)
+
+	def stop_backup(self):
+		out = self.psql.execute("SELECT pg_stop_backup()")
+		self._logger.debug(out)
+					
 	
 class ClusterDir(object):
 	
