d4950a99a683298675e7caa3a7ed364dadfbd9a4
Dmitry Korsakov <dmitry@scalr.com>
2011-11-16 17:00:31 +0000

    Fix for mongod is_running

--- src/scalarizr/services/mongodb.py
+++ src/scalarizr/services/mongodb.py
@@ -517,7 +517,7 @@ class Mongod(object):
 		self.keyfile = keyfile
 		self.cli = MongoCLI(port=port)
 		self.port = port
-		self.sock = initdv2.SockParam(self.port or ROUTER_DEFAULT_PORT)
+		self.sock = initdv2.SockParam(self.port or REPLICA_DEFAULT_PORT)
 		
 	@classmethod
 	def find(cls, mongo_conf=None, keyfile=None):
