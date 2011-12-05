a71f18916ce9c5e49122811a93580f0f1ef23e0f
Dmitry Korsakov <dmitry@scalr.com>
2011-12-05 12:32:06 +0000

    fix for moving ph_hba on centos

--- src/scalarizr/services/postgresql.py
+++ src/scalarizr/services/postgresql.py
@@ -223,6 +223,7 @@ class PostgreSql(BaseService):
 			self.config_dir.move_to(self.unified_etc_path)
 			make_symlinks(os.path.join(mpoint, STORAGE_DATA_DIR), self.unified_etc_path)
 			self.postgresql_conf = PostgresqlConf.find(self.config_dir)
+			self.pg_hba_conf = PgHbaConf.find(self.config_dir)
 		
 
 	def _set(self, key, obj):
