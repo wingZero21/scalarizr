1d1e190288528ab7fc5763d51407e405762ddace
Marat Komarov <marat@scalr.com>
2011-12-16 09:54:00 +0000

    * Added sa-east-1 region

--- src/scalarizr/platform/ec2/__init__.py
+++ src/scalarizr/platform/ec2/__init__.py
@@ -43,14 +43,17 @@ class Ec2Platform(Ec2LikePlatform):
 		"us-east-1" 		: "ec2.amazonaws.com",
 		"us-west-1" 		: "ec2.us-west-1.amazonaws.com",
 		"us-west-2" 		: "ec2.us-west-2.amazonaws.com",
+		"sa-east-1"			: "ec2.sa-east-1.amazonaws.com",
 		"eu-west-1" 		: "ec2.eu-west-1.amazonaws.com",
 		"ap-southeast-1" 	: "ec2.ap-southeast-1.amazonaws.com",
 		"ap-northeast-1" 	: "ec2.ap-northeast-1.amazonaws.com"
+		
 	}
 	s3_endpoints = {
 		'us-east-1' 		: 's3.amazonaws.com',
 		'us-west-1' 		: 's3-us-west-1.amazonaws.com',
 		'us-west-2' 		: 's3-us-west-2.amazonaws.com',
+		"sa-east-1"			: "s3-sa-east-1.amazonaws.com",
 		'eu-west-1' 		: 's3.amazonaws.com',
 		'ap-southeast-1' 	: 's3-ap-southeast-1.amazonaws.com',
 		'ap-northeast-1' 	: 's3-ap-northeast-1.amazonaws.com'
