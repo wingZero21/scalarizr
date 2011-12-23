0afa7883d618fbac149c2c5fe8c5819d8e21b2f5
Marat Komarov <marat@scalr.com>
2011-12-23 16:26:41 +0000

    * Added sysinfo API skeleton

--- src/scalarizr/api/sysinfo.py
+++ src/scalarizr/api/sysinfo.py
@@ -6,7 +6,55 @@ Created on Nov 25, 2011
 Pluggable API to get system information similar to SNMP, Facter(puppet), Ohai(chef)
 '''
 
+import os
+
+from scalarizr import rpc
+
+
 class SysInfoAPI(object):
 
-	def query(self, expr):
+	def add_extension(self, extension):
+		# @todo: export each callable public attribute into self 
+		raise NotImplemented()
+	
+	@rpc.service_method
+	def fqdn(self, fqdn=None):
+		# get or set hostname
+		raise NotImplemented()
+
+	@rpc.service_method
+	def block_devices(self):
+		# __UCD-DISKIO-MIB.py
+		raise NotImplemented()
+		return ['sda1', 'loop0']
+	
+	@rpc.service_method
+	def uname(self):
+		raise NotImplemented()
+	
+	@rpc.service_method
+	def lsb_release(self):
+		raise NotImplemented()
+	
+	@rpc.service_method
+	def pythons(self):
+		raise NotImplemented()
+
+	@rpc.service_method
+	def cpu_info(self):
+		raise NotImplemented()
+	
+	@rpc.service_method
+	def load_average(self):
+		return os.getloadavg()
+
+	@rpc.service_method
+	def disk_stats(self):
+		# later
 		pass
+	
+	@rpc.service_method
+	def bandwidth(self):
+		# __IF-MIB.py
+		pass
+	
