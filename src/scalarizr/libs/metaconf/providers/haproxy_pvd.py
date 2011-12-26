8402482a587ff6d6f23036c78edd1c2718c909cc
Nick Demyanchuk <spike@scalr.com>
2011-12-26 09:57:57 +0000

    # First steps of metaconf refactoring

--- src/scalarizr/libs/metaconf/providers/haproxy_pvd.py
+++ src/scalarizr/libs/metaconf/providers/haproxy_pvd.py
@@ -1,5 +1,6 @@
 __author__ = 'Nicholas Demyanchuk'
 
+from . import FormatProvider
 from .ini_pvd import IniFormatProvider
 from .. import MetaconfError
 from ..utils import quote, unquote
@@ -27,6 +28,20 @@ class HaproxyFormatProvider(IniFormatProvider):
 		self._indent = ''
 
 
+	def create_element(self, etree, path, value):
+		el = FormatProvider.create_element(self, etree, path, value)
+		parent_path = os.path.dirname(path)
+		if os.path.dirname(parent_path) not in ('.', ''):
+			raise MetaconfError('Maximum nesting level for haproxy format is 2')
+		elif parent_path in ('.', ''):
+			if etree.find(path) is not None:
+				raise MetaconfError("Ini file can't contain two identical sections")
+			el.attrib['mc_type'] = 'section'
+		else:
+			el.attrib['mc_type'] = 'option'
+		return el
+
+
 	def read_section(self, line, root):
 		if not hasattr(self, '_section_re'):
 			self._section_re = re.compile(self._section_re_string)
@@ -38,9 +53,8 @@ class HaproxyFormatProvider(IniFormatProvider):
 				self._cursect.append(comment)
 			self._cursect = ET.SubElement(root, quote(res.group('section_name')))
 			self._cursect.attrib['mc_type'] = 'section'
-			value = res.group('value')
-			value = value or ''
-			self._cursect.attrib['value'] = quote(value.strip())
+			value = res.group('value') or ''
+			self._cursect.attrib['value'] = value.strip()
 			return True
 		return False
 
@@ -48,9 +62,8 @@ class HaproxyFormatProvider(IniFormatProvider):
 	def write_section(self, fp, node):
 		if node.attrib.has_key('mc_type') and node.attrib['mc_type'] == 'section':
 			fp.write(unquote(node.tag))
-			value = unquote(node.attrib['value']) if node.attrib.has_key('value') else ''
-			if value:
-				fp.write(' ' + value)
+			value = node.attrib['value']
+			fp.write(' ' + value)
 			fp.write('\n')
 			self._indent = '\t'
 			self.write(fp, node, False)
@@ -69,11 +82,13 @@ class HaproxyFormatProvider(IniFormatProvider):
 			if res.group('comment'):
 				comment = ET.Comment(res.group('comment')[1:])
 				self._cursect.append(comment)
+
 			new_opt = ET.SubElement(self._cursect, quote(res.group('option').strip()))
-			value = res.group('value')
-			if value:
-				new_opt.text = quote(value.strip())
 			new_opt.attrib['mc_type'] = 'option'
+
+			value = res.group('value') or ''
+			new_opt.attrib['value'] = value.strip()
+
 			return True
 		return False
 
@@ -81,7 +96,7 @@ class HaproxyFormatProvider(IniFormatProvider):
 	def write_option(self, fp, node):
 		if node.attrib.has_key('mc_type') and node.attrib['mc_type'] == 'option':
 			fp.write("\t" + unquote(node.tag))
-			value = node.text if node.text else ''
+			value = node.attrib['value']
 			if value:
 				fp.write('\t' + unquote(value))
 			fp.write('\n')
