a4f6ea8865c8a75d50e4c9b49a28f247b6d3b401
Marat Komarov <marat@scalr.com>
2012-03-30 17:20:52 +0000

    update scalarizr crypto key in proper place

--- src/scalarizr/handlers/lifecycle.py
+++ src/scalarizr/handlers/lifecycle.py
@@ -203,11 +203,11 @@ class LifeCycleHandler(scalarizr.handlers.Handler):
 			snmp_port = self._cnf.rawini.get(config.SECT_SNMP, config.OPT_PORT),
 			snmp_community_name = self._cnf.rawini.get(config.SECT_SNMP, config.OPT_COMMUNITY_NAME)
 		), broadcast=True)
-		bus.fire("before_host_init", msg)		
-		self.send_message(msg, wait_ack=True)
-		
+		bus.fire("before_host_init", msg)
 		# Update key file
 		self._cnf.write_key(self._cnf.DEFAULT_KEY, new_crypto_key)		
+		self.send_message(msg, wait_ack=True)
+		
 
 		bus.cnf.state = ScalarizrState.INITIALIZING
 		bus.fire("host_init")
