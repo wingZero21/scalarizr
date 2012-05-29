10b66d4db4736a153edf0055b8ffa48cc3fc319f
Marat Komarov <marat@scalr.com>
2012-05-29 09:57:55 +0000

    * Refactored tests structure

---
+++ tests/unit/scalarizrtests/handlers/test_mysql.py
@@ -0,0 +1,298 @@
+'''
+Created on 17.06.2010
+
+@author: spike
+'''
+
+import unittest
+import os
+import signal, pexpect, re
+from scalarizr.util import init_tests, ping_service
+from scalarizr.bus import bus
+from scalarizr.handlers import mysql
+from scalarizr.util import system, initd, disttool, cryptotool, configtool
+from scalarizr.platform.ec2 import Ec2Platform
+from subprocess import Popen, PIPE, STDOUT
+import time, shutil, hashlib, pwd
+
+
+class _Volume:
+	def __init__(self):
+		self.id = 'test_id'
+		self._messages = []
+		
+class _MysqlHandler(mysql.MysqlHandler):
+	def _create_storage(self, vol_id, mnt_point):
+		pass
+	def _detach_delete_volume(self, volume):
+		pass
+	def _create_volume_from_snapshot(self, snap_id):
+		return _Volume()
+	def _mount_device(self, devname, mnt_point):
+		pass
+	def send_message(self, message):
+		self._messages.append(message)
+	def _create_ebs_snapshot(self):
+		pass
+	def _take_master_volume(self, volume_id, ec2_conn=None):
+		return _Volume()
+	
+LOCAL_IP = '12.34.56.78'
+
+class Test(unittest.TestCase):
+
+	def setUp(self):
+		system('mkdir /mnt/tmpdir')
+		system('rsync -a /var/lib/mysql/ /mnt/tmpdir/mysql-data')
+		system('rsync -a /var/log/mysql/binarylog/ /mnt/tmpdir/mysql-misc')
+		system('cp -pr /etc/mysql/ /tmp/mysqletc/')
+		system('cp -pr /var/lib/mysql /tmp/mysqldata/')
+
+	def tearDown(self):
+		initd.stop("mysql")
+		system('cp /etc/mysql/my.cnf /tmp/etc'+time.strftime("%d%b%H%M%S", time.gmtime()))
+		system('rm -rf /etc/mysql/')
+		system('rm -rf /var/lib/mysql')
+		system('cp -pr /tmp/mysqletc/ /etc/mysql/ ')
+		system('cp -pr /tmp/mysqldata/ /var/lib/mysql ') 
+		system('rm -rf /tmp/mysql*')
+		system('rm -rf /mnt/dbstorage/*')
+		system('rm -rf /mnt/tmpdir/*')
+		initd.start("mysql")
+		config = bus.config
+		section	= configtool.get_behaviour_section_name(mysql.BEHAVIOUR)
+		try:
+			config.remove_option(section, mysql.OPT_ROOT_USER)
+			config.remove_option(section, mysql.OPT_ROOT_PASSWORD)
+			config.remove_option(section, mysql.OPT_REPL_USER)
+			config.remove_option(section, mysql.OPT_REPL_PASSWORD)
+			config.remove_option(section, mysql.OPT_STAT_USER)
+			config.remove_option(section, mysql.OPT_STAT_PASSWORD)
+		except:
+			pass
+
+	def test_users(self):
+		bus.queryenv_service = _QueryEnv()
+		bus.platform = _Platform()
+		handler = _MysqlHandler()
+		handler._stop_mysql()
+		root_password, repl_password, stat_password = handler._add_mysql_users(mysql.ROOT_USER,
+																			   mysql.REPL_USER,
+																			   mysql.STAT_USER)
+		handler._stop_mysql()
+		myd = handler._start_mysql_skip_grant_tables()
+		for user, password in {mysql.ROOT_USER: root_password,
+							   mysql.REPL_USER: repl_password,
+							   mysql.STAT_USER: stat_password}.items():			
+			myclient = Popen(["/usr/bin/mysql"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+			out,err = myclient.communicate("SELECT Password from mysql.user where User='"+user+"'")
+			hashed_pass = re.search ('Password\n(.*)', out).group(1)
+			self.assertEqual(hashed_pass, mysql_password(password))
+		os.kill(myd.pid, signal.SIGTERM)		   
+		
+	def test_create_snapshot(self):
+		bus.queryenv_service = _QueryEnv()
+#		bus.platform = Ec2Platform()
+		bus.platform = _Platform()
+		handler = _MysqlHandler()
+		
+		root_password, repl_password, stat_password = handler._add_mysql_users(mysql.ROOT_USER,
+																			   mysql.REPL_USER,
+																			   mysql.STAT_USER)
+		handler._move_mysql_dir('log_bin', '/var/log/mysql/binarylog/binary.log', 'mysqld')
+		handler._replication_init()
+		snap_id, log_file, log_pos = handler._create_snapshot(mysql.ROOT_USER, root_password)
+		sql = pexpect.spawn('/usr/bin/mysql -u' + mysql.ROOT_USER + ' -p' + root_password)
+		sql.expect('mysql>')
+		sql.sendline('SHOW MASTER STATUS;\n')
+		sql.expect('mysql>')
+		lines = sql.before
+		# Retrieve log file and log position
+		log_row = re.search(re.compile('^\|\s*([\w-]*\.\d*)\s*\|\s*(\d*)', re.MULTILINE), lines)
+		if log_row:
+			true_log_file = log_row.group(1)
+			true_log_pos = log_row.group(2)
+		sql.close()
+		self.assertEqual(log_file, true_log_file)
+		self.assertEqual(log_pos, true_log_pos)
+		file = open('/etc/mysql/farm-replication.cnf')
+		self.assertEqual('[mysqld]\nserver-id\t\t=\t1\nmaster-connect-retry\t\t=\t15\n', file.read())
+		file.close()
+		
+	
+	def test_on_before_host_up(self):
+		bus.queryenv_service = _QueryEnv()
+#		bus.platform = Ec2Platform()
+		bus.platform = _Platform()
+		handler = _MysqlHandler()
+		message = _Message()
+		config = bus.config
+		config.set(configtool.SECT_GENERAL, mysql.OPT_REPLICATION_MASTER, '1')
+		handler.on_before_host_up(message)
+		self.assertTrue(os.path.exists('/mnt/dbstorage/mysql-data'))
+		self.assertTrue(os.path.exists('/mnt/dbstorage/mysql-misc'))
+		mysql_user	= pwd.getpwnam("mysql")
+		self.assertEqual(mysql_user.pw_uid, os.stat('/mnt/dbstorage/mysql-data')[4])
+		self.assertEqual(mysql_user.pw_gid, os.stat('/mnt/dbstorage/mysql-data')[5])
+		self.assertEqual(mysql_user.pw_uid, os.stat('/mnt/dbstorage/mysql-misc')[4])
+		self.assertEqual(mysql_user.pw_gid, os.stat('/mnt/dbstorage/mysql-misc')[5])
+		handler.on_before_host_up(message)
+		datadir, log_bin = extract_datadir_and_log()
+		self.assertEqual(datadir, '/mnt/dbstorage/mysql-data/')
+		self.assertEqual(log_bin, '/mnt/dbstorage/mysql-misc/binlog.log')
+		
+	def test_on_mysql_newmaster_up(self):
+		bus.queryenv_service = _QueryEnv()
+		bus.platform = _Platform()
+		config = bus.config
+		sect_name = configtool.get_behaviour_section_name(mysql.BEHAVIOUR)
+		config.set(sect_name, mysql.OPT_REPLICATION_MASTER, '0')
+		handler = _MysqlHandler()
+		root_pass, repl_pass, stat_pass = handler._add_mysql_users(mysql.ROOT_USER, mysql.REPL_USER, mysql.STAT_USER)
+		handler._update_config( {mysql.OPT_ROOT_PASSWORD : root_pass,
+								 mysql.OPT_REPL_PASSWORD : repl_pass,
+								 mysql.OPT_STAT_PASSWORD : stat_pass})
+		message = _Message()
+		if disttool.is_redhat_based():
+			daemon = "/usr/libexec/mysqld"
+		else:
+			daemon = "/usr/sbin/mysqld"
+		initd.stop("mysql")
+		myd = Popen([daemon, '--defaults-file=/etc/mysql2/my.cnf', '--skip-grant-tables'], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+		ping_service(LOCAL_IP, 3306, 5)
+		myclient = pexpect.spawn('/usr/bin/mysql -h'+LOCAL_IP)
+		myclient.expect('mysql>')
+		repl_password = re.sub('[^\w]','', cryptotool.keygen(20))
+		sql = "update mysql.user set password = PASSWORD('"+repl_password+"') where user = '"+mysql.REPL_USER+"';"
+		myclient.sendline(sql)
+		myclient.expect('mysql>')
+		result = myclient.before
+		if re.search('ERROR', result):
+			os.kill(myd.pid, signal.SIGTERM)
+			raise BaseException("Cannot update user", result)
+		myclient.sendline('FLUSH TABLES WITH READ LOCK;')
+		myclient.expect('mysql>')
+#		system('cp -pr /var/lib/mysql /var/lib/backmysql')
+#		system('rm -rf /var/lib/mysql && cp -pr /var/lib/mysql2 /var/lib/mysql')
+		myclient.sendline('SHOW MASTER STATUS;')
+		myclient.expect('mysql>')
+		# retrieve log file and position
+		try:
+			master_status = myclient.before.split('\r\n')[4].split('|')
+		except:
+			raise BaseException("Cannot get master status")
+		finally:
+			myclient.sendline('UNLOCK TABLES;')
+			os.kill(myd.pid, signal.SIGTERM)
+		myd = Popen([daemon, '--defaults-file=/etc/mysql2/my.cnf'], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+		ping_service(LOCAL_IP, 3306, 5)
+		message.log_file = master_status[1].strip()
+		message.log_pos = master_status[2].strip()
+		message.repl_user = mysql.REPL_USER
+		message.repl_password = repl_password
+		message.root_password = root_pass
+		handler.on_Mysql_NewMasterUp(message)
+		os.kill(myd.pid, signal.SIGTERM)
+		initd.stop("mysql")
+		system ('rm -rf /var/lib/mysql && cp -pr /var/lib/backmysql /var/lib/mysql && rm -rf /var/lib/backmysql')
+		config.set(sect_name, mysql.OPT_REPLICATION_MASTER, '1')
+		
+	def test_on_before_host_up_slave_ebs(self):
+		bus.queryenv_service = _QueryEnv()
+		bus.platform = _Platform()
+		message = _Message()
+		config = bus.config
+		sect_name = configtool.get_behaviour_section_name(mysql.BEHAVIOUR)
+		config.set(sect_name, mysql.OPT_REPLICATION_MASTER, '0')
+		bus.queryenv_service.storage = 'ebs'
+		handler = _MysqlHandler()
+		handler.on_before_host_up(message)
+		config.set(sect_name, mysql.OPT_REPLICATION_MASTER, '1')
+		datadir, log_bin = extract_datadir_and_log()
+		self.assertEqual(datadir, '/mnt/dbstorage/mysql-data/')
+		self.assertEqual(log_bin, '/mnt/dbstorage/mysql-misc/binlog.log')
+		
+	def test_on_before_host_up_slave_eph(self):
+		bus.queryenv_service = _QueryEnv()
+		bus.platform = _Platform()
+		message = _Message()
+		config = bus.config
+		sect_name = configtool.get_behaviour_section_name(mysql.BEHAVIOUR)
+		config.set(sect_name, mysql.OPT_REPLICATION_MASTER, '0')
+		bus.queryenv_service.storage = 'eph'
+		handler = _MysqlHandler()
+		handler.on_before_host_up(message)
+		config.set(sect_name, mysql.OPT_REPLICATION_MASTER, '1')
+		datadir, log_bin = extract_datadir_and_log()
+		self.assertEqual(datadir, '/mnt/dbstorage/mysql-data/')
+		self.assertEqual(log_bin, '/mnt/dbstorage/mysql-misc/binlog.log')
+		
+	def test_on_Mysql_PromoteToMaster(self):
+		bus.queryenv_service = _QueryEnv()
+		bus.platform = _Platform()
+		config = bus.config
+		sect_name = configtool.get_behaviour_section_name(mysql.BEHAVIOUR)
+		config.set(sect_name, mysql.OPT_REPLICATION_MASTER, '0')
+		message = _Message()
+		message.root_password = '123'
+		message.repl_password = '456'
+		message.stat_password = '789'
+		handler = _MysqlHandler()
+		handler.on_Mysql_PromoteToMaster(message)
+		
+def mysql_password(str):
+	pass1 = hashlib.sha1(str).digest()
+	pass2 = hashlib.sha1(pass1).hexdigest()
+	return "*" + pass2.upper()
+
+def extract_datadir_and_log():
+		if disttool.is_redhat_based():
+			my_cnf_file = "/etc/my.cnf"
+		else:
+			my_cnf_file = "/etc/mysql/my.cnf"
+		file = open(my_cnf_file)
+		mycnf = file.read()
+		file.close()
+		datadir = re.search(re.compile('^\s*datadir\s*=\s*(.*)$', re.MULTILINE), mycnf).group(1)
+		log_bin = re.search(re.compile('^\s*log_bin\s*=\s*(.*)$', re.MULTILINE), mycnf).group(1)
+		return datadir, log_bin
+
+class _Bunch(dict):
+			__getattr__, __setattr__ = dict.get, dict.__setitem__
+
+class _QueryEnv:
+	
+	def __init__(self):
+		self.storage = 'ebs'
+	
+	def list_role_params(self, role_name):
+		return _Bunch(
+			mysql_data_storage_engine = self.storage,
+			mysql_master_ebs_volume_id = 'test-id',
+			ebs_snap_id = 'test_snap_id'
+			)
+		
+class _Platform:
+	def get_instance_id(self):
+		pass
+	
+	def get_block_device_mapping(self):
+		return {'ephemeral0' : 'eph0'}
+
+class _Message:
+	def __init__(self):
+		self.repl_user = None
+		self.repl_password = None
+		self.log_file = None
+		self.log_pos  = None
+		self.mysql_repl_user = None
+		self.mysql_repl_password = None
+		self.mysql_stat_password = None
+		self.mysql_stat_user = None
+		self.local_ip = LOCAL_IP
+		self.mysql_replication_master = 1
+
+if __name__ == "__main__":
+	init_tests()
+	unittest.main()
+
---
+++ tests/unit/scalarizrtests/storage/test.py
@@ -0,0 +1,337 @@
+'''
+Created on Nov 25, 2010
+
+@author: marat
+'''
+import unittest
+
+from scalarizr.util import system2 as system, firstmatched
+from scalarizr.storage import Volume, Snapshot, Storage, StorageError, VolumeProvider,\
+	VolumeConfig
+from scalarizr.storage.util.loop import mkloop
+from scalarizr.storage.eph import EphSnapshot
+from scalarizr.storage.fs import FileSystem
+from szr_unittest import switch_reset_db
+
+import os
+import time
+from random import randint
+
+
+class TestMkloop(unittest.TestCase):
+	filename = None
+	loop = None
+	SIZE = 100
+	
+	def setUp(self):
+		self.loop = None
+		self.filename = '/tmp/loop%s' % randint(11, 99)
+	
+	def tearDown(self):
+		if self.loop:
+			system('/sbin/losetup -d %s' % self.loop, shell=True)
+		if os.path.exists(self.filename):
+			os.remove(self.filename)
+
+	def test_create_quick(self):
+		t1 = time.time()
+		self.loop = mkloop(self.filename, size=self.SIZE, quick=True)
+		t2 = time.time()
+		self.assertTrue(t2 - t1 < 0.1)
+		self.assert_size()
+		
+	def test_create(self):
+		t1 = time.time()
+		self.loop = mkloop(self.filename, size=self.SIZE, quick=False)
+		t2 = time.time()
+		self.assertTrue(t2 - t1 > 0.1)
+		self.assert_size()
+		
+	def test_create_on_exiting_file(self):
+		system('/bin/dd if=/dev/zero of=%s bs=1M seek=%d count=1' % (self.filename, self.SIZE-1), shell=True)
+		self.loop = mkloop(self.filename)
+		self.assert_size()
+		
+	def assert_size(self):
+		self.assertEqual(os.path.getsize(self.filename), self.SIZE * 1024 * 1024)
+		
+class TestResourceManagement(unittest.TestCase):
+	mgr = None
+	def setUp(self):
+		self.mgr = Storage
+		self.mgr._fs_drivers = {}
+	
+	def tearDown(self):
+		pass
+	
+	def test_singleton_fs(self):
+		myfs = 'myfs'
+		class FSDriver(FileSystem):
+			name = myfs
+		self.mgr.explore_filesystem(myfs, FSDriver)
+		o1 = self.mgr.lookup_filesystem(myfs)
+		self.assertTrue(isinstance(o1, FSDriver))
+		self.assertEqual(o1, self.mgr.lookup_filesystem(myfs))
+
+	def test_lookup_std_fs(self):
+		fs = self.mgr.lookup_filesystem('ext4')
+		self.assertEqual(fs.name, 'ext4')
+	
+		
+class TestVolume(unittest.TestCase):
+	def setUp(self):
+		self.device = mkloop('/tmp/testvolume', size=100)
+		self.mpoint = '/mnt/storage'
+		if not os.path.exists(self.mpoint):
+			os.makedirs(self.mpoint)
+	
+	def tearDown(self):
+		if self.device:
+			system(('/sbin/losetup', '-d', self.device))
+		if os.path.exists(self.mpoint):
+			os.removedirs(self.mpoint)
+	
+	def test(self):
+		vol = Volume(self.device, self.mpoint)
+		
+		# When trying to do filesystem operations without having a fs on volume ValueError raises
+		self.assertRaises(StorageError, vol.freeze)
+		self.assertRaises(StorageError, vol.unfreeze)
+		
+		# After creating a filesystem ValueError dissapears
+		vol.mkfs('ext3')
+		self.assertTrue(vol.fstype, 'ext3')
+		vol.freeze()
+		vol.unfreeze()
+		
+		# When volume is mounted without mpoint specify, it gets from volume mpoint property
+		self.assertFalse(vol.mounted())
+		vol.mount()
+		self.assertTrue(vol.mpoint, self.mpoint)
+		self.assertTrue(vol.mounted())
+				
+		# When volume is unmounted, volume mpoint property remains
+		vol.umount()
+		self.assertEqual(vol.mpoint, self.mpoint)
+		self.assertFalse(vol.mounted())
+		
+		snap = vol.snapshot(description='test snap')
+		self.assertTrue(isinstance(snap, Snapshot))
+		self.assertEqual(snap.description, 'test snap')
+
+	def test_config(self):
+		vol = Volume('/dev/smd0', '/mnt/trace', 'xfs')
+		cnf = vol.config()
+		self.assertTrue(isinstance(cnf, dict))
+		self.assertTrue('device' in cnf)
+		self.assertEqual(cnf['device'], '/dev/smd0')
+		self.assertTrue('type' in cnf)
+		self.assertEqual(cnf['type'], 'base')
+
+class TestStorageProviders(unittest.TestCase):
+	_save_snap_pvd = None
+	_save_vol_pvd = None
+	_save_pvds = None
+
+	class MyPvd(VolumeProvider):
+		type = 'myvol'
+	
+	def setUp(self):
+		self._save_pvds = Storage.providers.copy()
+		Storage.providers.clear()
+		self._save_snap_pvd, Storage.default_snap_provider = Storage.default_snap_provider, None
+		self._save_vol_pvd, Storage.default_vol_provider = Storage.default_vol_provider, None
+
+	def tearDown(self):
+		Storage.providers = self._save_pvds
+		Storage.default_snap_provider = self._save_snap_pvd
+		Storage.default_vol_provider = self._save_vol_pvd
+	
+	def test_explore_provider(self):
+		Storage.explore_provider(self.MyPvd)
+		self.assertFalse(Storage.default_snap_provider)
+		self.assertFalse(Storage.default_vol_provider)
+		self.assertTrue(isinstance(Storage.lookup_provider(self.MyPvd.type), self.MyPvd))
+	
+	def test_explore_default_provider(self):
+		Storage.explore_provider(self.MyPvd, True)
+		self.assertFalse(Storage.default_snap_provider)
+		self.assertEqual(Storage.default_vol_provider, self.MyPvd.type)
+		
+		self.assertTrue(isinstance(Storage.lookup_provider(self.MyPvd.type), self.MyPvd))
+		self.assertTrue(isinstance(Storage.lookup_provider(), self.MyPvd))
+		
+	def test_explore_default_provider2(self):
+		Storage.explore_provider(self.MyPvd, default_for_snap=True)
+		self.assertEqual(Storage.default_snap_provider, self.MyPvd.type)
+		self.assertFalse(Storage.default_vol_provider)
+		
+		self.assertTrue(isinstance(Storage.lookup_provider(self.MyPvd.type), self.MyPvd))
+		self.assertTrue(isinstance(Storage.lookup_provider(None, True), self.MyPvd))
+	
+	def test_snapshot_factory(self):
+		Storage.providers = self._save_pvds		
+		pvd = Storage.lookup_provider('eph')
+		snap = pvd.snapshot_factory('hom')
+		self.assertEqual(snap.type, 'eph')
+		self.assertEqual(snap.description, 'hom')
+		self.assertTrue(isinstance(snap, EphSnapshot))
+	
+	
+class TestStorageCreate(unittest.TestCase):
+	class Vol(Volume):
+		def __init__(self, *args, **kwargs):
+			if kwargs:
+				for k, v in kwargs.items():
+					setattr(self, k, v)
+			Volume.__init__(self, *args, **kwargs)
+			
+	class VolPvd(VolumeProvider):
+		type = 'myvol'
+	
+	def setUp(self):
+		self.VolPvd.vol_class = self.Vol
+		Storage.explore_provider(self.VolPvd)
+
+	def tearDown(self):
+		pass
+	
+	def test_create_by_string_args(self):
+		vol = Storage.create('/dev/sdb')
+		self.assertEqual(vol.devname, '/dev/sdb')
+
+	def test_create_over_disk(self):
+		vol = Storage.create(type='myvol', device='/dev/lvolume', disk='/dev/sdb')
+		self.assertEqual(vol.disk.devname, '/dev/sdb')
+		
+		vol = Storage.create(
+			type='myvol', 
+			device='/dev/ldevice2',
+			disk=dict(
+				type='myvol',
+				device='/dev/sdb',
+				param1='value1'
+			)
+		)
+		self.assertEqual(vol.disk.devname, '/dev/sdb')
+		self.assertEqual(vol.disk.param1, 'value1')
+		
+	def test_create_vol_container(self):
+		vol = Storage.create(
+			type='myvol',
+			device='/dev/gp0',
+			disks=('/dev/sdb', dict(type='myvol', device='/dev/sdd'))
+		)
+		self.assertEqual(len(vol.disks), 2)
+		self.assertEqual(vol.disks[0].devname, '/dev/sdb')
+		self.assertEqual(vol.disks[1].devname, '/dev/sdd')
+		self.assertEqual(vol.disks[1].type, 'myvol')
+	
+	def test_create_from_snapshot(self):
+		vol = Storage.create(
+			snapshot=dict(
+				type='base',
+				device='/dev/sdb',
+				mpoint='/mnt/dbstorage',
+				fstype='xfs'
+			)
+		)
+		self.assertEqual(vol.devname, '/dev/sdb')
+		self.assertEqual(vol.mpoint, '/mnt/dbstorage')
+		
+		vol = Storage.create(
+			device='/dev/sdd',
+			snapshot=dict(
+				type='myvol',
+				device='/dev/lvol',
+				param1='value1',
+				param2='value2'
+			)
+		)
+		self.assertEqual(vol.devname, '/dev/sdd')
+		self.assertEqual(vol.type, 'myvol')
+		self.assertEqual(vol.param1, 'value1')
+
+class SnapshotFieldsTest(unittest.TestCase):
+	def test_base_volume(self):
+		device = '/dev/sdo'
+		mpoint = '/mnt/media-server-flvs'
+		fstype = 'ext4'
+		
+		vol = Storage.create(device=device, mpoint=mpoint, fstype=fstype)
+		snap = vol.snapshot('snap #00')
+		
+		snap_cnf = snap.config()
+		vol_cnf = vol.config()
+		self.assertEqual(snap_cnf['type'], vol.type)
+		self.assertEqual(snap_cnf['mpoint'], vol.mpoint)
+		self.assertEqual(snap_cnf['device'], vol.device)
+
+
+	def test_with_ignores(self):
+		class VolConfig(VolumeConfig):
+			vg = None
+			base64_whatever = None
+			only_in_volume_config = None
+			only_in_snapshot_config = None
+			
+		class Vol(VolConfig, Volume):
+			_ignores = ('only_in_snapshot_config')
+
+		class Snap(VolConfig, Snapshot):
+			_ignores = ('only_in_volume_config',)
+		
+		class VolPvd(VolumeProvider):
+			type = 'mimimi'
+			vol_class = Vol
+			snap_class = Snap
+		Storage.explore_provider(VolPvd)	
+		
+		
+		vol = Storage.create(
+			type='mimimi', 
+			device='/dev/sdo', 
+			vg='vg0', 
+			only_in_volume_config='4u', 
+			only_in_snapshot_config='4s'
+		)
+		snap = vol.snapshot()
+		snap_cnf = snap.config()
+		vol_cnf = vol.config()
+
+		self.assertFalse('only_in_volume_config' in snap_cnf)
+		self.assertEqual(snap_cnf['vg'], 'vg0')
+		
+		self.assertFalse('only_in_snapshot_config' in vol_cnf)
+		self.assertTrue(vol_cnf['base64_whatever'] is None)
+		
+class VolumeTableTest(unittest.TestCase):
+	def setUp(self):
+		Storage.maintain_volume_table = True
+		switch_reset_db()
+		
+	def tearDown(self):
+		pass
+	
+	def test_1(self):
+		v1 = Storage.create(device='/dev/sdo')
+		v2 = Storage.create(device='/dev/sdm')
+		table = Storage.volume_table()
+		self.assertEqual(len(table), 2)
+		v1row = firstmatched(lambda row: row['device'] == '/dev/sdo', table)
+		self.assertTrue(v1row)
+		self.assertEqual(v1row['volume_id'], v1.id)
+		self.assertEqual(v1row['device'], v1.device)
+		self.assertEqual(v1row['type'], v1.type)
+		self.assertEqual(v1row['state'], 'attached')
+		
+		v2.detach()
+		table = Storage.volume_table()
+		self.assertEqual(len(table), 2)
+		v2row = firstmatched(lambda row: row['device'] == '/dev/sdm', table)
+		self.assertEqual(v2row['state'], 'detached')
+
+		
+if __name__ == "__main__":
+	#import sys;sys.argv = ['', 'Test.testName']
+	unittest.main()
