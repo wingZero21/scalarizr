9f3a830cc8b38b37226455c24d1a1ff6a1d5f6b8
Dmitry Korsakov <dmitry@scalr.com>
2012-05-31 16:33:05 +0000

    fix for asserting tuples

--- src/scalarizr/services/mysql.py
+++ src/scalarizr/services/mysql.py
@@ -230,9 +230,9 @@ class MySQLClient(object):
 	
 	def user_exists(self, login, host):
 		ret = self.fetchone("select User,Host from mysql.user where User='%s' and Host='%s'" % (login, host))
-		LOG.debug('user_exists query returned value: %s' % str(ret))
+		LOG.debug('user_exists query returned value: %s for user %s on host %s' % (str(ret), login, host))
 		#return True if ret and ret['Host']==host and ret['User']==login else False
-		return True if ret==(login,host) else False 
+		return True if len(ret)==2 and ret[0]==login and ret[1]==host else False 
 		
 	def flush_privileges(self):
 		return self.fetchone("FLUSH PRIVILEGES")
