b5ea408e9f22b7c2f2fff5c64d1a749a161866ae
Nick Demyanchuk <spike@scalr.com>
2012-06-15 13:18:30 +0000

    # Raid fixed (recreation from config)

--- src/scalarizr/storage/raid.py
+++ src/scalarizr/storage/raid.py
@@ -77,19 +77,19 @@ class RaidVolumeProvider(VolumeProvider):
 		self._lvm = Lvm2()
 		self._logger = logging.getLogger(__name__)
 		self._lvm_backup_filename = '/tmp/lvm_backup'
-		
-	
+
+
 	def create(self, **kwargs):
 		'''
 		@param disks: Raid disks
 		@type disks: list(Volume)
-		
+
 		@param level: Raid level 0, 1, 5, 10 - are valid values
 		@type level: int
-		
+
 		@param vg: Volume group over RAID PV
 		@type vg: str|dict
-		
+
 		'''
 
 		if kwargs.get('lvm_group_cfg'):
@@ -101,14 +101,42 @@ class RaidVolumeProvider(VolumeProvider):
 			vg_name = kwargs['vg']['name']
 			del kwargs['vg']['name']
 			vg_options = kwargs['vg']
-			self._lvm.create_pv(raid_pv)		
-			kwargs['vg'] = self._lvm.create_vg(vg_name, (raid_pv,), **vg_options)
-			kwargs['device'] = self._lvm.create_lv(vg_name, extents='100%FREE')
+
+			self._create_pv(raid_pv)
+			kwargs['vg'] = self._create_vg(vg_name, raid_pv, **vg_options)
+			kwargs['device'] = self._create_lv(vg_name, kwargs.get('device'))
 			kwargs['raid_pv'] = raid_pv
 			kwargs['pv_uuid'] = self._lvm.pv_info(raid_pv).uuid
 			kwargs['lvm_group_cfg'] = lvm_group_b64(kwargs['vg'])
 			volume = super(RaidVolumeProvider, self).create(**kwargs)
 		return volume
+
+
+	def _create_pv(self, device, uuid=None):
+		try:
+			self._lvm.pv_info(device)
+		except LookupError:
+			self._logger.debug('Creating physical volume. %s ' % (uuid and 'uuid=%s' % uuid or ''))
+			self._lvm.create_pv(device, uuid=uuid)
+
+
+	def _create_vg(self, name, device, **options):
+		try:
+			self._lvm.vg_info(name)
+			return '/dev/%s' % name
+		except:
+			return self._lvm.create_vg(name, (device,), **options)
+
+
+	def _create_lv(self, vg_name, device=None):
+		if device:
+			try:
+				lvi = self._lvm.lv_info(device)
+				return lvi.path
+			except:
+				pass
+
+		return self._lvm.create_lv(vg_name, extents='100%FREE')
 
 
 	def create_from_snapshot(self, **kwargs):
@@ -128,12 +156,8 @@ class RaidVolumeProvider(VolumeProvider):
 		else:
 			raid_pv	= self._mdadm.assemble([vol.devname for vol in kwargs['disks']])
 
-		try:
-			self._lvm.pv_info(raid_pv)
-		except LookupError:
-			pv_uuid = kwargs.get('pv_uuid')
-			self._logger.debug('Recreating physical volume with uuid=%s' % pv_uuid)
-			self._lvm.create_pv(raid_pv, uuid=pv_uuid)
+		pv_uuid = kwargs.get('pv_uuid')
+		self._create_pv(raid_pv, pv_uuid )
 
 		lvm_raw_backup = binascii.a2b_base64(kwargs['lvm_group_cfg'])
 		write_file(self._lvm_backup_filename, lvm_raw_backup, logger=logger)
@@ -156,7 +180,7 @@ class RaidVolumeProvider(VolumeProvider):
 							pv_uuid = pv_info.uuid,
 							lvm_group_cfg = kwargs['lvm_group_cfg'])
 
-	
+
 	@devname_not_empty
 	def create_snapshot(self, vol, snap, **kwargs):
 
@@ -235,8 +259,8 @@ class RaidVolumeProvider(VolumeProvider):
 
 	def destroy(self, vol, force=False, **kwargs):
 		super(RaidVolumeProvider, self).destroy(vol, force, **kwargs)
-		
-		remove_disks=kwargs.get('remove_disks') 
+
+		remove_disks=kwargs.get('remove_disks')
 		if not vol.detached:
 			self._remove_lvm(vol, force)
 			# Check if sleeping is necessary
@@ -249,7 +273,7 @@ class RaidVolumeProvider(VolumeProvider):
 
 
 
-	@devname_not_empty			
+	@devname_not_empty
 	def detach(self, vol, force=False):
 		self._logger.debug('Detaching volume %s' % vol.devname)
 		super(RaidVolumeProvider, self).detach(vol, force)
@@ -271,5 +295,5 @@ class RaidVolumeProvider(VolumeProvider):
 		self._lvm.remove_vg(vol.vg)
 		self._lvm.remove_pv(vol.raid_pv)
 		vol.device = None
-	
+
 Storage.explore_provider(RaidVolumeProvider)
