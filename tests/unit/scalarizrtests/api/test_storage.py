ddbfa35453a44038684a9f2de2f412bed5eb7d37
Nick Demyanchuk <spike@scalr.com>
2012-06-20 15:21:33 +0000

    * Updated storage test

--- tests/unit/scalarizrtests/api/test_storage.py
+++ tests/unit/scalarizrtests/api/test_storage.py
@@ -5,35 +5,42 @@ Created on Jun 19, 2012
 '''
 
 from scalarizr.api import storage as storage_api
+from scalarizr.api.storage import threading
 from scalarizr import storage as storage_lib
 from scalarizr import handlers
-import threading
 
 import mock
 from nose.tools import assert_equal, assert_true
 
-class ThreadMock(mock.Mock):
-	def start(self):
-		ThreadMock.call_args[1]['target']()
 
 class TestStorageAPI(object):
 	
 	@classmethod
 	def setup_class(cls):
-		storage_lib.Storage = mock.Mock()
-		handlers.operation = mock.MagicMock()
-		threading.Thread = ThreadMock
+		threading.Thread = mock.MagicMock()
+
+		def thread_side_effect(*args, **kwargs):
+			target = kwargs.get('target')
+			ret = mock.MagicMock()
+			ret.start.side_effect = lambda: target()
+			return ret
+
+		threading.Thread.side_effect = thread_side_effect
+
 	
 	def setup(self):
+		storage_lib.Storage = mock.MagicMock()
+		handlers.operation = mock.MagicMock()
 		self.api = storage_api.StorageAPI()
 		self.sample_vol = storage_lib.Volume('/dev/xvde1', '/mnt/services', 'ext4', 'base')
 
 	
 	def test_create_async(self):
-		
 		storage_lib.Storage.create.return_value = self.sample_vol
-		ret = self.api.create(storage_config={'type': 'ebs', 'size': 1}, async=True)
-		assert_true(ret)
-		
+		self.api.create(volume_config={'type': 'ebs', 'size': 1}, async=True)
+		handlers.operation.assert_called_once_with(name='Create volume')
+
+
+
 		
 		
