b88d1164ed45e9d1bcba6f4ec832ef2220212467
Marat Komarov <marat@scalr.com>
2012-11-02 09:34:23 +0000

    * Added: eph adapter to old storage

--- src/scalarizr/storage2/volumes/eph.py
+++ src/scalarizr/storage2/volumes/eph.py
@@ -10,6 +10,9 @@ from scalarizr.util import filetool
 from scalarizr.storage2 import cloudfs
 from scalarizr.storage2.volumes import base
 
+# until transfer will be ready
+from scalarizr.storage import eph as old_eph
+
 
 LOG = logging.getLogger(__name__)
 
@@ -46,6 +49,7 @@ class EphVolume(base.Volume):
 				cloudfs_dir=cloudfs_dir, **kwds)
 
 		self._transfer = None
+		self._lvm_volume = None
 		self.features.update({'restore': True})
 
 
@@ -54,7 +58,13 @@ class EphVolume(base.Volume):
 		# snap should be applied after layout: download and extract data.
 		# this could be done on already ensured volume. 
 		# Example: resync slave data
-		if not hasattr(self, '_lvm_volume'):
+
+		if not self._lvm_volume:
+			if isinstance(self.disk, basestring) and \
+					self.disk.startswith('/dev/sd'):
+				self.disk = storage2.volume(
+						type='ec2_ephemeral', 
+						name='ephemeral0')
 			self._lvm_volume = storage2.volume(
 					type='lvm',
 					pvs=[self.disk],
@@ -170,6 +180,54 @@ class EphSnapshot(base.Snapshot):
 		return self.UNKNOWN
 
 
-storage2.volume_types['eph'] = EphVolume
+class EphVolumeAdapter(EphVolume):
+	
+	def __init__(self, **kwds):
+		super(EphVolume, self).__init__(**kwds)
+		self.snap_backend = self._config.pop('cloudfs_dir')
+		self._eph_vol = None
+		self._eph_pvd = old_eph.EphVolumeProvider()
+		
+		
+	def _ensure(self):
+		if not self._eph_vol:
+			config = self.snap.config() if self.snap else self.config()
+			disk = storage2.volume(config['disk'])
+			if disk.device.startswith('/dev/sd'):
+				disk = storage2.volume(
+						type='ec2_ephemeral', 
+						name='ephemeral0')
+				disk.ensure()
+				config['disk'] = disk.device
+
+			if self.snap:
+				self._eph_vol = self._eph_pvd.create_from_snapshot(**config)
+				self.snap = None
+			else:
+				self._eph_vol = self._eph_pvd.create(**config)
+			
+			self.device = self._eph_vol.device
+		
+		
+	def _snapshot(self, description, tags, **kwds):
+		eph_snap = self._eph_pvd.snapshot_factory(description, 
+												tags=tags)
+		eph_snap = self._eph_pvd.create_snapshot(self._eph_vol, eph_snap)
+		
+		snap = storage2.snapshot(type='eph')
+		snap._config.update(eph_snap.config())
+		return snap
+	
+	
+	def _destroy(self, force, **kwds):
+		self._eph_pvd.destroy(self._eph_vol, force, **kwds)	
+	
+
+	def _detach(self, force, **kwds):
+		self._eph_pvd.detach(self._eph_vol, force)
+
+
+#storage2.volume_types['eph'] = EphVolume
+storage2.volume_types['eph'] = EphVolumeAdapter
 storage2.snapshot_types['eph'] = EphSnapshot
 
