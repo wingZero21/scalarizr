d6e1108e3f838927b51cd161af19ba8f40930df8
Nick Demyanchuk <spike@scalr.com>
2012-11-09 16:27:38 +0000

    # Yet another fix (stupid 2.5 python)

--- src/scalarizr/storage2/volumes/raid.py
+++ src/scalarizr/storage2/volumes/raid.py
@@ -143,9 +143,9 @@ class RaidVolume(base.Volume):
 
 		else:
 			raid_device = mdadm.findname()
-			mdadm.mdadm('create', raid_device, *disks_devices,
-						force=True, level=self.level, assume_clean=True,
-						raid_devices=len(disks_devices), metadata='default')
+			kwargs = dict(force=True, level=self.level, assume_clean=True,
+						  raid_devices=len(disks_devices), metadata='default')
+			mdadm.mdadm('create', raid_device, *disks_devices, **kwargs)
 			mdadm.mdadm('misc', raid_device, wait=True)
 
 			lvm2.pvcreate(raid_device, force=True)
