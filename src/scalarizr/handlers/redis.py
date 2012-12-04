3d30e73ad88c5ad770191e321501ab6fc66bc27f
Dmitry Korsakov <dmitry@scalr.com>
2012-12-04 17:50:42 +0000

    Fixed ports casting

--- src/scalarizr/handlers/redis.py
+++ src/scalarizr/handlers/redis.py
@@ -187,7 +187,7 @@ class RedisHandler(ServiceCtlHandler, handlers.FarmSecurityMixin):
 					if param not in redis_data:
 						break
 					else:
-						ports = redis_data['ports']
+						ports = map(int, redis_data['ports'])
 						passwords = redis_data['passwords']
 						num_processes = int(redis_data['num_processes'])
 
@@ -247,7 +247,7 @@ class RedisHandler(ServiceCtlHandler, handlers.FarmSecurityMixin):
 					num_processes = 1
 
 					if 'ports' in redis_data:
-						ports = redis_data['ports']
+						ports = map(int, redis_data['ports'])
 						del redis_data['ports']
 
 					if 'passwords' in redis_data:
@@ -273,7 +273,7 @@ class RedisHandler(ServiceCtlHandler, handlers.FarmSecurityMixin):
 
 					if self.default_service.running:
 						self.default_service.stop('Treminating default redis instance')
-					LOG.debug("Got ports: %s . And passwords: %s" % (str(ports), str(passwords)))
+						
 					self.redis_instances = redis.RedisInstances(self.is_replication_master, self.persistence_type)
 					ports = ports or [redis.DEFAULT_PORT,]
 					passwords = passwords or [self.get_main_password(),]
