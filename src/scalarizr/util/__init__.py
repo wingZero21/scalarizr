0d22214d9b2a158b29280ae8279d3d3e07b466c9
Marat Komarov <marat@scalr.com>
2012-12-11 09:40:05 +0000

    * fixed: parametrized shebang doesn't work

--- src/scalarizr/util/__init__.py
+++ src/scalarizr/util/__init__.py
@@ -329,8 +329,12 @@ def read_shebang(path=None, script=None):
 
 	shebang = re.search(re.compile('^#!(\S+.+)'), first_line)
 	if shebang:
-		return shebang.group(1)
+		return split_strip(shebang.group(1))[0]
 	return None
+
+def split_strip(value, separator=' '):
+	return map(string.strip, value.split(separator))
+
 
 def parse_size(size):
 	"""
