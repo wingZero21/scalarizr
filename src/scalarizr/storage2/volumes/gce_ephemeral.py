52bde683de99630f3b56dcbff29840b0d19f9c08
Nick Demyanchuk <spike@scalr.com>
2012-12-12 15:45:21 +0000

    * [GCE] Google ephemeral devices link v1beta13 support added

--- src/scalarizr/storage2/volumes/gce_ephemeral.py
+++ src/scalarizr/storage2/volumes/gce_ephemeral.py
@@ -10,7 +10,7 @@ class GceEphemeralVolume(base.Volume):
 			name=name, **kwds)
 
 	def _ensure(self):
-		device = '/dev/disk/by-id/google-%s' % self.name
+		device = '/dev/disk/by-id/scsi-0Google_EphemeralDisk_%s' % self.name
 		if not os.path.exists(device):
 			msg = "Device '%s' not found" % device
 			raise storage2.StorageError(msg)
