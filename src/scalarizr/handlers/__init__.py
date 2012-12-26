3a67291d43cb3e92e2520985243d411e71940ef4
Marat Komarov <marat@scalr.com>
2012-12-26 17:10:46 +0000

    Fixed: AttributeError: 'MemcachedHandler' object has no attribute '_platform'

--- src/scalarizr/handlers/__init__.py
+++ src/scalarizr/handlers/__init__.py
@@ -675,11 +675,11 @@ class FarmSecurityMixin(object):
 			self._logger.warn("iptables is not enabled. ports %s won't be protected by firewall" %  (ports, ))
 		
 	def __on_init(self):
-		self.__insert_iptables_rules()
 		bus.on(
 			reload=self.__on_reload
 		)
-		self.__on_reload()		
+		self.__on_reload()
+		self.__insert_iptables_rules()		
 	
 	def __on_reload(self):
 		self._queryenv = bus.queryenv_service
