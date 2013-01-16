7913bbef4f86f4efc3493517de009dd0ad40f0e4
Marat Komarov <marat@scalr.com>
2013-01-16 13:09:37 +0000

    * Fixed: NameError: global name 'which' is not defined

--- src/scalarizr/linux/__init__.py
+++ src/scalarizr/linux/__init__.py
@@ -12,6 +12,20 @@ class LinuxError(util.PopenError):
 	pass
 
 
+def which(exe):
+	(path, _) = osmod.path.split(exe)
+	if osmod.access(exe, osmod.X_OK):
+		return exe
+
+	default_path = '/bin:/sbin:/usr/bin:/usr/sbin:/usr/libexec:/usr/local/bin'
+
+	for path in osmod.environ.get('PATH', default_path).split(osmod.pathsep):
+		full_path = osmod.path.join(path, exe)
+		if osmod.access(full_path, osmod.X_OK):
+			return full_path
+	return None
+
+
 def system(*args, **kwds):
 	kwds['exc_class'] = LinuxError
 	kwds['close_fds'] = True
@@ -185,19 +199,3 @@ def build_cmd_args(executable=None, short=None, long=None, params=None):
 	if params:
 		ret += list(params)
 	return map(str, ret)
-
-
-def which(exe):
-	if exe and exe.startswith('/') and \
-			osmod.access(exe, osmod.X_OK):
-		return exe
-	exe = osmod.path.basename(exe)
-	path = '/bin:/sbin:/usr/bin:/usr/sbin:/usr/libexec:/usr/local/bin'
-	if osmod.environ.get('PATH'):
-		path += ':' + osmod.environ['PATH']
-	for p in set(path.split(osmod.pathsep)):
-		full_path = osmod.path.join(p, exe)
-		if osmod.access(full_path, osmod.X_OK):
-			return full_path
-	return None
-
