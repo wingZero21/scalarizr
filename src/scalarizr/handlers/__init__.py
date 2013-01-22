cc8f0a576fd0fbe1a1803a7f62815c06c2d4ce5d
Marat Komarov <marat@scalr.com>
2013-01-22 16:41:37 +0000

    * Added: mysqld >= 5.0 now detected as mysql2, percona >= 5.0 as percona behavior

--- src/scalarizr/handlers/__init__.py
+++ src/scalarizr/handlers/__init__.py
@@ -266,12 +266,12 @@ class Handler(object):
 					handlers.append(config.BuiltinBehaviours.MONGODB)
 				elif name == 'apache' and Version('2.0') <= version < Version('2.3'):
 					handlers.append(config.BuiltinBehaviours.APP)
-				elif name == 'mysql' and Version('5.0') <= version < Version('5.5'):
-					handlers.append(config.BuiltinBehaviours.MYSQL)
-				elif name == 'mysql' and Version('5.5') <= version and str_ver and 'Percona' in str_ver:
-					handlers.append(config.BuiltinBehaviours.PERCONA)
-				elif name == 'mysql' and Version('5.5') <= version:
-					handlers.append(config.BuiltinBehaviours.MYSQL2)
+				elif name == 'mysql' and Version('5.0') <= version < Version('5.6'):
+					if 'Percona' in str_ver:
+						handlers.append(config.BuiltinBehaviours.PERCONA)
+					else:
+						handlers.append(config.BuiltinBehaviours.MYSQL2)
+
 		return handlers
 
 
