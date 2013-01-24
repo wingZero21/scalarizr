970b465072773b74e746148bfee799a2d2eebe9d
Nick Demyanchuk <spike@scalr.com>
2013-01-24 10:20:13 +0000

    # Temporary fix for ephemeral devices on gce (pvcreate uses another device name)

--- src/scalarizr/storage2/volumes/gce_ephemeral.py
+++ src/scalarizr/storage2/volumes/gce_ephemeral.py
@@ -11,7 +11,9 @@ class GceEphemeralVolume(base.Volume):
 
 	def _ensure(self):
 		self._check_attr('name')
-		device = '/dev/disk/by-id/%s' % self.name
+		if self.name.startswith('google-'):
+			self.name = self.name[7:]
+		device = '/dev/%s' % self.name
 		if not os.path.exists(device):
 			msg = "Device '%s' not found" % device
 			raise storage2.StorageError(msg)
