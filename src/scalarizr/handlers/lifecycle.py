e3afe17a768c58a24882c0e600c3dd23f7c74002
Marat Komarov <marat@scalr.com>
2013-02-20 13:28:16 +0000

    Fixed: TypeError: NoneType object is not iterable

--- src/scalarizr/handlers/lifecycle.py
+++ src/scalarizr/handlers/lifecycle.py
@@ -334,8 +334,9 @@ class LifeCycleHandler(scalarizr.handlers.Handler):
 			###
 			if 'volumes' in message.body:
 				self._logger.debug('HIR volumes:\n%s' % message.volumes)
+				volume = message.volumes or []
 				hostup_msg.body['volumes'] = []
-				for vol_info in message.volumes:
+				for vol_info in volumes:
 					vol = storage2_volume(**vol_info)
 					vol.ensure()
 					vol_config = vol.config()
