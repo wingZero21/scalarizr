36254abf353a7beeb5ba1bc583b9d8ead25c4aff
Nick Demyanchuk <spike@scalr.com>
2013-02-27 13:59:03 +0200

    * [Storage2] Raid from snapshot - use disks AND snapshots for underlying disks

--- src/scalarizr/storage2/volumes/raid.py
+++ src/scalarizr/storage2/volumes/raid.py
@@ -72,6 +72,7 @@ class RaidVolume(base.Volume):
 						'snapshot' in self.snap['disks'][0]
 		if self.snap:
 			disks = []
+			snaps = []
 			try:
 				# @fixme: pv should be based on a disk config with snapshot taste
 				# @todo: create disks concurrently
@@ -79,7 +80,19 @@ class RaidVolume(base.Volume):
 					if self._v1_compat:
 						disk_snap = disk_snap['snapshot']
 					snap = storage2.snapshot(disk_snap)
-					disks.append(snap.restore())
+					snaps.append(snap)
+
+
+				if self.disks and len(self.disks) != len(snaps):
+					raise storage2.StorageError('Volume disks count is not equal to '
+												'snapshot disks count')
+
+				# Mixing snapshots to self.volumes (if exist) or empty volumes
+				disks = self.disks or [storage2.volume(type=s['type']) for s in snaps]
+
+				for disk, snap in zip(disks, snaps):
+					disk['snap'] = snap
+
 			except:
 				for disk in disks:
 					disk.destroy()
