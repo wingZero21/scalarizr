435e95861e42c3cf10bda6d6fd63c3fdcedb0542
Gennadiy Stas <uty@scalr.com>
2013-03-19 13:55:52 +0200

    TODO added

--- src/scalarizr/api/mongodb.py
+++ src/scalarizr/api/mongodb.py
@@ -15,6 +15,7 @@ class MongoDBAPI:
     @rpc.service_method
     def reset_password(self):
         """ Reset password for Mongo user 'scalr'. Return new password  """
+        #TODO: review and finish this method
         new_password = pwgen(10)
         mdb = mongodb_svc.MongoDB()
         mdb.cli.create_or_update_admin_user(mongodb_svc.SCALR_USER,
