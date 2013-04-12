47406c66b683cfeeb9afc5841a7243c09a307eee
Vladimir Ryazanov <vladimir@scalr.com>
2013-04-12 19:13:14 +0300

    i have a server

--- tests/acceptance/haproxy.py
+++ tests/acceptance/haproxy.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import logging
 import time
+import errno
 
 from lettuce import step, world, before, after
 
@@ -11,10 +12,10 @@ from scalarizr.libs.bases import Task
 LOG = logging.getLogger(__name__)
 
 
-class Server(Task):
+class SocketServer(Task):
 
     def __init__(self, port):
-        super(Server, self).__init__()
+        super(SocketServer, self).__init__()
 
         self._address = ('', port)
 
@@ -39,38 +40,62 @@ class Server(Task):
         return str(self._address)
 
     def _handle(self, client_sock, address):
-        LOG.info("[%s] Got connection from %s", self, address)
-        client_sock.send(str(self))
+        if not self._killed:
+            LOG.info("[%s] Got connection from %s", self, address)
+            client_sock.send(str(self))
+        else:
+            # we expect client_sock to be the one created by self._kill()
+            LOG.debug("[%s] Recieved kill connection from %s", self, address)
+            client_sock.send("debug: being killed")
 
     def _kill(self):
         self._killed = True
+        # make sock.accept() return
+        try:
+            socket.socket().connect(self._address)
+        except socket.error, e:
+            if e.errno == errno.ECONNREFUSED:
+                pass
+
+    def communicate(self):
+        """ Make a connection to this server and read the response """
+        # error: [Errno 111] Connection refused - random port
+        # error: [Errno 104] Connection reset by pier - dying server
+        sock = socket.socket()
+        sock.connect(self._address)
+
+        response = sock.recv(1024)  # FIXME: indefinite block if _handle has 
+                                    # crashed; using communicate from main thread
+                                    # allows to CTRL+C this
+        LOG.info("[Communicate] %s replied: %s", self._address, response)
+        return response
 
 
-def communicate(port):
-    address = ('', port)
+class Server(SocketServer, threading.Thread):
 
-    sock = socket.socket()
-    sock.connect(address)
+    port0 = 27000
 
-    response = sock.recv(1024)  # FIXME: indefinite block if the server has 
-                                # crashed; using communicate from main thread
-                                # allows to CTRL+C this
-    LOG.info("[Communicate] %s replied: %s", address, response)
-    return response
+    def __init__(self):
+        super(Server, self).__init__(self.port0 + len(world.servers))
+        threading.Thread.__init__(self)  # bases.Task breaks super() chain
+
+        world.servers.append(self)
+
+
+@before.each_scenario
+def setup(scenario):
+    world.servers = []
+
+
+@after.each_scenario
+def teardown(scenario):
+    map(lambda x: x.kill(), world.servers)
+    map(lambda x: x.join(), world.servers)
 
 
 @step("i have a server")
 def i_have_a_server(step):
-    
-    server = Server(27015)
-
-    thread = threading.Thread(target=server.run)
-    thread.start()
-
-    time.sleep(1)
-    server.kill()
-    communicate(27015)
-
+    Server()
 
 
 @step("i add proxy")
