2874bbfe0a2900674788d8b12c1f60f2e5fdb266
Marat Komarov <marat@scalr.com>
2013-04-30 12:21:52 +0300

    Also set order based on a weight for bus['init'] and bus['start'] listeners

--- src/scalarizr/handlers/__init__.py
+++ src/scalarizr/handlers/__init__.py
@@ -326,6 +326,8 @@ class MessageListener:
 				return cmp(cls_weight(a), cls_weight(b))
 
 			self._handlers_chain = list(reversed(sorted(hds, sort_fn)))
+			bus._listeners['init'] = list(reversed(sorted(bus._listeners['init'], sort_fn)))
+			bus._listeners['start'] = list(reversed(sorted(bus._listeners['init'], sort_fn)))
 			LOG.debug("Message handlers chain:\n%s", pprint.pformat(self._handlers_chain))
 	
 	def __call__(self, message, queue):
