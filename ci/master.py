56089a8b3e62dd0634129550acedd395bc94d71e
Marat Komarov <marat@scalr.com>
2013-05-29 14:12:01 +0300

    Updated CI instructions

--- ci/master.py
+++ ci/master.py
@@ -10,40 +10,41 @@ project = __opts__['project']
 
 
 c['schedulers'].append(AnyBranchScheduler(
-        name=project,
-        change_filter=ChangeFilter(project=project, category='default'),
-        builderNames=['{0} source'.format(project)]
+    name=project,
+    change_filter=ChangeFilter(project=project, category='default'),
+    builderNames=['{0} source'.format(project)]
 ))
 
 
 c["schedulers"].append(Triggerable(
-        name="{0} packaging".format(project),
-        builderNames=["deb_packaging", "rpm_packaging"]
+    name="{0} packaging".format(project),
+    builderNames=["deb_packaging", "rpm_packaging"]
 ))
 
 
 def push_to_github(__opts__):
-    cwd = 'sandboxes/{0}/svn2git'.format(project)
     return [
-            MasterShellCommand(
-                    command="""
-                    cd sandboxes/{0}/svn2git
-                    svn2git --rebase --verbose
-                    git push origin master""".format(project),
-                    description='Pushing commit to GitHub',
-                    descriptionDone='Push commit to GitHub (trunk)'),
+        MasterShellCommand(
+            command="""
+            cd sandboxes/{0}/public 
+            git pull --rebase private master
+            git push origin master""".format(project),
+            description='Pushing commit to GitHub',
+            descriptionDone='Push commit to GitHub (trunk)'),
     ]
 
 
 c['builders'].append(dict(
-        name='{0} source'.format(project),
-        slavenames=['ubuntu1004'],
-        factory=BuildFactory(steps=
-                buildsteps.svn(__opts__) +
-                buildsteps.bump_version(__opts__, setter='cat > src/scalarizr/version') +
-                buildsteps.source_dist(__opts__) +
-                buildsteps.trigger_packaging(__opts__) +
-                buildsteps.to_repo(__opts__, types=["deb", "rpm"]) +
-                push_to_github(__opts__)
-        )
+    name='{0} source'.format(project),
+    slavenames=['ubuntu1004'],
+    factory=BuildFactory(steps=
+        #buildsteps.svn(__opts__) +
+        #buildsteps.bump_version(__opts__, setter='cat > src/scalarizr/version') +
+        buildsteps.git(__opts__) +
+        buildsteps.bump_version_for_git(__opts__, setter='cat > src/scalarizr/version') +
+        buildsteps.source_dist(__opts__) +
+        buildsteps.trigger_packaging(__opts__) + 
+        buildsteps.to_repo(__opts__, types=["deb", "rpm"]) +
+        push_to_github(__opts__)
+    )
 ))
