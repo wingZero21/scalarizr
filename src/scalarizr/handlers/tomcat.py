ef354863b36f2b823ccae015ea4647a30ded7af9
Marat Komarov <marat@scalr.com>
2013-06-04 18:44:59 +0300

    Configure Tomcat SSL

--- src/scalarizr/handlers/tomcat.py
+++ src/scalarizr/handlers/tomcat.py
@@ -21,7 +21,24 @@ class TomcatHandler(handlers.Handler, handlers.FarmSecurityMixin):
             init=self.on_init, 
             start=self.on_start
         )
-        self.service = initdv2.ParametrizedInitScript('tomcat', '/etc/init.d/tomcat')
+        self.tomcat_version = None
+        self.config_dir = None
+        self.init_script_path = None
+
+        if linux.os.debian_family:
+            if (linux.os['name'] == 'Ubuntu' and linux.os['version'] >= (12, 4)) or \
+                (linux.os['name'] == 'Debian' and linux.os['version']  >= (7, 0)):
+                self.tomcat_version = 7
+            else:
+                self.tomcat_version = 6
+            self.config_dir = '/etc/tomcat{0}'.format(self.tomcat_version)
+            self.init_script_path = '/etc/init.d/tomcat{0}'.format(self.tomcat_version)
+        else:
+            self.tomcat_version = 6
+            self.config_dir = '/etc/tomcat'
+            self.init_script_path = '/etc/init.d/tomcat'
+
+        self.service = initdv2.ParametrizedInitScript('tomcat', self.init_script_path)
 
     def on_init(self):
         bus.on(
@@ -39,21 +56,46 @@ class TomcatHandler(handlers.Handler, handlers.FarmSecurityMixin):
                 Messages.HOST_DOWN) and 'tomcat' in behaviour
 
     def on_host_init_response(self, hir_message):
-        if not os.path.exists('/etc/init.d/tomcat'):
-            pkgs = []
+        if not os.path.exists(self.service.initd_script):
+            tomcat = 'tomcat{0}'.format(self.tomcat_version)
+            pkgs = [tomcat]
             if linux.os.debian_family:
-                if (linux.os['name'] == 'Ubuntu' and linux.os['version'] >= (12, 4)) or \
-                    (linux.os['name'] == 'Debian' and linux.os['version']  >= (7, 0)):
-                    tomcat = 'tomcat7'
-                else:
-                    tomcat = 'tomcat6'
-                pkgs = [tomcat, '{0}-admin'.format(tomcat)]
+                pkgs.append('{0}-admin'.format(tomcat))
             elif linux.os.redhat_family or linux.os.oracle_family:
-                tomcat = 'tomcat6'
-                pkgs = [tomcat, '{0}-admin-webapps'.format(tomcat)]
+                pkgs.append('{0}-admin-webapps'.format(tomcat))
             for pkg in pkgs:
                 pkgmgr.installed(pkg)
+        pkgmgr.installed('augeas-tools' if linux.os.debian_family else 'augeas')
 
     def on_before_host_up(self, message):
+        load_lens = [
+            'set /augeas/load/Xml/incl[last()+1] "{config_dir}/*.xml"',
+            'load',
+            'defvar service /files/{0}/server.xml/Server/Service' .format(self.config_dir)                       
+        ]
+        
+        # Enable SSL
+        augscript = '\n'.join(load_lens + [
+            'print $service/Connector/*/port'
+        ])
+        augscript = augscript.format(config_dir=self.config_dir)
+        out = linux.system(('augtool',), stdin=augscript)[1]
+        if not '8443' in out:
+            self.service.stop()
+            augscript = '\n'.join(load_lens + [
+                'defnode connector $service/Connector[last()+1]',
+                'defvar attrs $connector/#attribute'
+                'set $attrs/port 8443',
+                'set $attrs/protocol "HTTP/1.1"',
+                'set $attrs/SSLEnabled true',
+                'set $attrs/maxThreads 150',
+                'set $attrs/scheme https',
+                'set $attrs/secure true',
+                'set $attrs/clientAuth false',
+                'set $attrs/sslProtocol TLS',
+                'save'
+            ])
+            linux.system(('augtool', ), stdin=augscript)
+
         self.service.start()
 
