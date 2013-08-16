8273ca6b521d67dae09643e998a5fca235707c66
Marat Komarov <marat@scalr.com>
2013-08-16 17:42:07 +0300

    Fixed: create_from_temlate_if_missing doesnt work for CloudStack volumes

--- src/scalarizr/handlers/block_device.py
+++ src/scalarizr/handlers/block_device.py
@@ -116,12 +116,7 @@ class BlockDeviceHandler(handlers.Handler):
 				LOG.info('Ensuring %s volume %s', vol.type, dict(vol))
 				try:
 					vol.ensure(mount=bool(vol.mpoint), mkfs=True)
-				#except storage2.VolumeNotExistsError, e:
-				except:
-					et, e, _ = sys.exc_info()
-					LOG.debug('class: %s, value: %s', et, e)
-					LOG.debug('template: %s, from_template_if_missing: %s', template, from_template_if_missing)
-
+				except storage2.VolumeNotExistsError, e:
 					if template and from_template_if_missing == '1':
 						vol = storage2.volume(**template)
 						LOG.warn('Volume %s not exists, re-creating %s volume from config: %s', 
