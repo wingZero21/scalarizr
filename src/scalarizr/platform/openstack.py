5f8bb4e06a4c0d1f35a9b95eb0e0877294ece54c
Marat Komarov <marat@scalr.com>
2013-09-27 14:26:38 +0300

    SCALARIZR-1135, workaround certification validation error, this should fix volumes

--- src/scalarizr/platform/openstack.py
+++ src/scalarizr/platform/openstack.py
@@ -56,7 +56,9 @@ class CinderWrapper(OpenstackServiceWrapper):
                                     self.tenant,
                                     auth_url=self.auth_url,
                                     region_name=self.region_name,
+                                    insecure=True,  # FIXME: SCALARIZR-1139
                                     **kwargs)
+
 
 
 class NovaWrapper(OpenstackServiceWrapper):
@@ -68,6 +70,7 @@ class NovaWrapper(OpenstackServiceWrapper):
                                   auth_url=self.auth_url,
                                   region_name=self.region_name,
                                   service_type=service_type,
+                                  insecure=True,  # FIXME: SCALARIZR-1139
                                   **kwargs)
 
 
