75dad0abdd703debc207e005d6061f06ddf314f4
Marat Komarov <marat@scalr.com>
2013-10-23 12:17:53 +0300

    SCALARIZR-1135 get back openstack rebundle

--- src/scalarizr/handlers/openstack/rebundle.py
+++ src/scalarizr/handlers/openstack/rebundle.py
@@ -1,12 +1,18 @@
 
+import os
 import sys
+import time
 import shutil
-import logging
 
-
+from scalarizr.node import __node__
 from scalarizr import linux, handlers
-from scalarizr.util import software
+from scalarizr.handlers import rebundle as rebundle_hdlr
+from scalarizr.util import software, system2, wait_until
 from scalarizr.messaging import Messages
+
+
+LOG = rebundle_hdlr.LOG
+
 
 def get_handlers():
     if linux.os.windows_family:
@@ -18,10 +24,7 @@ def get_handlers():
 class OpenstackRebundleWindowsHandler(handlers.Handler):
     logger = None
 
-    def __init__(self):
-        self._logger = logging.getLogger(__name__)
-
-    def accept(self, message, queue, behaviour=None, platform=None, os=None, dist=None):
+    def accept(self, message, queue, **kwds):
         return message.name == Messages.WIN_PREPARE_BUNDLE
 
     def on_Win_PrepareBundle(self, message):
@@ -41,10 +44,51 @@ class OpenstackRebundleWindowsHandler(handlers.Handler):
 
         except:
             e = sys.exc_info()[1]
-            self._logger.exception(e)
+            LOG.exception(e)
             last_error = hasattr(e, "error_message") and e.error_message or str(e)
             self.send_message(Messages.WIN_PREPARE_BUNDLE_RESULT, dict(
                 status = "error",
                 last_error = last_error,
                 bundle_task_id = message.bundle_task_id
             ))
+
+
+
+class OpenstackRebundleLinuxHandler(rebundle_hdlr.RebundleHandler):
+
+    def rebundle(self):
+        image_name = self._role_name + "-" + time.strftime("%Y%m%d%H%M%S")
+        nova = __node__['openstack']['new_nova_connection']
+        nova.connect()
+
+        server = nova.servers.get(__node__['openstack']['server_id'])
+        system2("sync", shell=True)
+        image = server.create_image(image_name)
+
+        result = [None]
+        def image_completed():
+            try:
+                result[0] = nova.images.get(image.id)
+                return result[0].status in ('ACTIVE', 'FAILED')
+            except:
+                e = sys.exc_info()[1]
+                if 'Unhandled exception occurred during processing' in str(e):
+                    return
+                raise
+        wait_until(image_completed, start_text='Polling image status', sleep=30)
+
+        image_id = result[0].id
+        if result[0].status == 'FAILED':
+            raise handlers.HandlerError('Image %s becomes FAILED', image_id)
+        LOG.info('Image %s completed and available for use!', image_id)
+        return image_id
+
+
+    def before_rebundle(self):
+        if os.path.exists('/etc/udev/rules.d/70-persistent-net.rules'):
+            shutil.move('/etc/udev/rules.d/70-persistent-net.rules', '/tmp')
+
+
+    def after_rebundle(self):
+        if os.path.exists('/tmp/70-persistent-net.rules'):
+            shutil.move('/tmp/70-persistent-net.rules', '/etc/udev/rules.d')
