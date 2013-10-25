3791261f789b1cc79aa91d895da1000448d5e4e6
Marat Komarov <marat@scalr.com>
2013-10-25 23:08:40 +0300

    SCALARIZR-1126 added ability to cancel operations with flag and cancel function

--- tests/unit/scalarizrtests/api/test_operation.py
+++ tests/unit/scalarizrtests/api/test_operation.py
@@ -2,6 +2,8 @@
 from scalarizr.api import operation
 
 import mock
+import time
+import threading
 from nose.tools import eq_, ok_
 
 import pprint
@@ -12,13 +14,16 @@ import pprint
 })
 class TestOperation(object):
 
-	def assert_op_result(self):
+	def assert_op_result(self, status=None):
 		from scalarizr.node import __node__
 		args, kwds = __node__['messaging'].send.call_args 
 		eq_(args[0], 'OperationResult')
-		return args, kwds	
+		if status:
+			eq_(kwds['body']['status'], status)
+		return kwds['body']
 
-	def test_serialize_error(self):
+
+	def test_result_error(self):
 		def fn_raises_error(op):
 			def deep():
 				op.logger.info('Im deep and going deeper')
@@ -31,29 +36,79 @@ class TestOperation(object):
 				open('/non/existed/file')
 			deep()
 
-		op = operation.OperationAPI().create('test_serialize_error', fn_raises_error)
+		op = operation.OperationAPI().create('test_result_error', fn_raises_error)
 		op.run()
 
-		_, kwds = self.assert_op_result()
-		eq_(kwds['body']['name'], 'test_serialize_error')
-		eq_(kwds['body']['status'], 'failed')
-		eq_(len(kwds['body']['logs']), 4)
-		ok_(kwds['body']['logs'][-1].endswith('Reason: [Errno 2] No such file or directory: \'/non/existed/file\''))
-		ok_('deep()' in kwds['body']['trace'])
-		ok_('deeper()' in kwds['body']['trace'])
-		ok_('abyss()' in kwds['body']['trace'])
+		result = self.assert_op_result('failed')
+		eq_(result['name'], 'test_result_error')
+		eq_(len(result['logs']), 4)
+		ok_(result['logs'][-1].endswith('Reason: [Errno 2] No such file or directory: \'/non/existed/file\''))
+		ok_('deep()' in result['trace'])
+		ok_('deeper()' in result['trace'])
+		ok_('abyss()' in result['trace'])
 
-	def test_serialize(self):
+
+	def test_result(self):
 		result_data = {
 			'embed': 'data'
 		}
 
 		def fn(op):
 			return result_data
-		op = operation.OperationAPI().create('test_serialize', fn)
+		op = operation.OperationAPI().create('test_result', fn)
 		op.run()
 
-		_, kwds = self.assert_op_result()
-		eq_(kwds['body']['status'], 'completed')
-		eq_(kwds['body']['result'], result_data)
+		result = self.assert_op_result('completed')
+		eq_(result['result'], result_data)
+
+
+	def check_cancel(self, asserts=None, op_func=None, cancel_func=None):
+		started = threading.Event()
+		canceled = threading.Event()
+
+		def fn(op):
+			ok_(not op.canceled)
+			started.set()
+			canceled.wait()
+			ok_(op.canceled)
+			if op_func:
+				op_func()
+
+		op = operation.OperationAPI().create('test_cancel', fn, cancel_func=cancel_func)
+		op.run_async()
+		started.wait()
+		op.cancel()
+		canceled.set()
+		time.sleep(.2) # Interrupt thread
+		if asserts:
+			asserts()
+
+	def test_cancel_error(self):
+		msg = 'Exception in op function after it was canceled'
+
+		def asserts():
+			result = self.assert_op_result('canceled')
+			eq_(msg, result['error'])
+			
+		self.check_cancel(asserts, op_func=mock.Mock(side_effect=Exception(msg)))
+
+
+	def test_cancel(self):
+		def asserts():
+			result = self.assert_op_result('canceled')
+			ok_(result['error'].startswith('User canceled'))
+
+		self.check_cancel(asserts)
+
+	def test_cancel_func(self):
+		cancel_func = mock.Mock()
+		def asserts():
+			eq_(cancel_func.call_count, 1)
+		self.check_cancel(asserts, cancel_func=cancel_func)
+
+	def test_cancel_func_error(self):
+		self.check_cancel(cancel_func=mock.Mock(side_effect=Exception))
+
+
+
 
