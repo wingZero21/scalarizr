d922898b33afb17c9eae04bdd0af5cd91a65ac36
Gennadiy Stas <uty@scalr.com>
2013-11-04 15:34:42 +0200

    fixed run permissions for scripts

--- src/scalarizr/handlers/script_executor.py
+++ src/scalarizr/handlers/script_executor.py
@@ -364,7 +364,7 @@ class Script(object):
             with open(self.exec_path, 'w') as fp:
                 fp.write(self.body.encode('utf-8'))
             if not linux.os.windows_family:
-                os.chmod(self.exec_path, stat.S_IREAD | stat.S_IEXEC)
+                os.chmod(self.exec_path, stat.S_IREAD | stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH)
 
         stdout = open(self.stdout_path, 'w+')
         stderr = open(self.stderr_path, 'w+')
