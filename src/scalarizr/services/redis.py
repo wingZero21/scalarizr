5733f96bfbe5b7048ee322fd27c0eb2448ef3e5d
Dmytro Korsakov <dmitry@scalr.com>
2013-11-19 14:41:38 +0200

    logging

--- src/scalarizr/services/redis.py
+++ src/scalarizr/services/redis.py
@@ -223,7 +223,8 @@ class RedisInstances(object):
     persistence_type = None
 
     def __init__(self, master=False, persistence_type='snapshotting', use_passwords=True):
-        LOG.debug("RedisInstances object initialized with persistence type %s" % persistence_type)
+        LOG.debug("RedisInstances object initialized with persistence type %s, repl_role: %s" % (
+            persistence_type, master,))
         self.master = master
         self.persistence_type = persistence_type
         self.use_passwords = use_passwords
