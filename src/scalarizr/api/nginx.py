8db0d4d0895088805917c92c4f90b91ac1406b6e
Gennadiy Stas <uty@scalr.com>
2013-12-09 14:00:53 +0200

    debug

--- src/scalarizr/api/nginx.py
+++ src/scalarizr/api/nginx.py
@@ -746,7 +746,7 @@ class NginxAPI(object):
 
         server_wide_template = grouped_templates.get('server')
         config.add('server', '')
-        if server_wide_template or 'dbg' in server_wide_template:
+        if server_wide_template and 'dbg' not in server_wide_template:
             # TODO: this is ugly. Find the way to read conf from string
             temp_file = self.proxies_inc_dir + '/temalate.tmp'
             with open(temp_file, 'w') as fp:
