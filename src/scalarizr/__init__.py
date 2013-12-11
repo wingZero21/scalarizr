6e525f077221ddce1feb759b72c50d60e83daece
Marat Komarov <marat@scalr.com>
2013-12-11 17:50:58 +0200

    SCALARIZR-1310 a try to do transparent WindowsService relocation

--- src/scalarizr/__init__.py
+++ src/scalarizr/__init__.py
@@ -1,2 +1,18 @@
 import os
+import sys
 __version__ = open(os.path.join(os.path.dirname(__file__), 'version')).read().strip()
+
+
+class ScalarizrModule(object):
+    def __getattr__(self, name):
+        if name in globals():
+            return globals()[name]
+        elif name == 'WindowsService':
+            import scalarizr.app
+            return scalarizr.app.WindowsService
+        else:
+            msg = "'{0}' has no attribute '{1}'".format(__name__, name)
+            raise AttributeError(msg)
+
+
+sys.modules[__name__] = ScalarizrModule()
