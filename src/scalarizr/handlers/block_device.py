11ec3985eb45a763dfbb3572111ef43ee225d61d
Marat Komarov <marat@scalr.com>
2013-12-12 23:19:02 +0200

    Fixed: recreate volume if missing stopped working

--- src/scalarizr/handlers/block_device.py
+++ src/scalarizr/handlers/block_device.py
@@ -112,13 +112,13 @@ class BlockDeviceHandler(handlers.Handler):
             for i in range(0, len(volumes)):
                 vol = volumes[i]
                 template = vol.pop('template', None)
-                from_template_if_missing = vol.pop('from_template_if_missing', None)
+                from_template_if_missing = vol.pop('from_template_if_missing', False)
                 vol = storage2.volume(**vol)
                 LOG.info('Ensuring %s volume %s', vol.type, dict(vol))
                 try:
                     vol.ensure(mount=bool(vol.mpoint), mkfs=True)
                 except storage2.VolumeNotExistsError, e:
-                    if template and from_template_if_missing == '1':
+                    if template and bool(int(from_template_if_missing)):
                         vol = storage2.volume(**template)
                         LOG.warn('Volume %s not exists, re-creating %s volume from config: %s', 
                                 str(e), vol.type, dict(vol))
