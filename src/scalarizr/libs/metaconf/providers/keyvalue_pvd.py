388f87ad4b76c5a19ccd8eb541352e79e691125a
Gennadiy Stas <uty@scalr.com>
2014-02-06 19:05:19 +0200

    fixed empty values

--- src/scalarizr/libs/metaconf/providers/keyvalue_pvd.py
+++ src/scalarizr/libs/metaconf/providers/keyvalue_pvd.py
@@ -6,6 +6,13 @@ from ..utils import unquote
 class KeyvalueFormatProvider(RedisFormatProvider):
     _opt_re_string = r'(?P<option>[^\s]+)=(?P<value>.*)(?P<comment>#(.*))?$'
     
+    def create_element(self, etree, path, value):
+        el = FormatProvider.create_element(self, etree, path, value)
+        if os.path.dirname(path) not in ('.', ''):
+            raise MetaconfError("key-value config format doesn't support nesting")
+        el.attrib['mc_type'] = 'option'
+        return el
+
     def write_option(self, fp, node):
         if node.attrib.has_key('mc_type') and node.attrib['mc_type'] == 'option':
             value = str(node.text if node.text else '')
