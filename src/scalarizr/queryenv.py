89cef2b36efdb687ed0e6032013027d22d774b28
Gennadiy Stas <uty@scalr.com>
2014-03-03 19:56:15 +0200

    typo fixed

--- src/scalarizr/queryenv.py
+++ src/scalarizr/queryenv.py
@@ -136,7 +136,7 @@ class QueryEnvService(object):
                                parameters,
                                self._read_list_farm_role_params_response,
                                log_response=False)
-        response_log_copy = dict(response)
+        response_log_copy = response.copy()
         try:
             del response_log_copy['chef']['validator_name']
             del response_log_copy['chef']['validator_key']
