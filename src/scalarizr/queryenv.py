1a75968ddf1ca3d13576649b1ade9f7073f65b3a
Gennadiy Stas <uty@scalr.com>
2014-03-04 13:50:47 +0200

    debug rebuild

--- src/scalarizr/queryenv.py
+++ src/scalarizr/queryenv.py
@@ -138,12 +138,12 @@ class QueryEnvService(object):
                                log_response=False)
         self._logger.debug("DEBUG QueryEnv response before remove: %s" % response)
         response_log_copy = response.copy()
-        try:
-            response_log_copy['chef'] = response['chef'].copy()
-            del response_log_copy['chef']['validator_name']
-            del response_log_copy['chef']['validator_key']
-        except (KeyError, TypeError):
-            pass
+        # try:
+        #     response_log_copy['chef'] = response['chef'].copy()
+        #     del response_log_copy['chef']['validator_name']
+        #     del response_log_copy['chef']['validator_key']
+        # except (KeyError, TypeError):
+        #     pass
         self._log_parsed_response(response_log_copy)
         self._logger.debug("DEBUG QueryEnv")
         self._log_parsed_response(response)  # DEBUG
