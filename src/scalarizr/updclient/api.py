a7c21862b902a9179f801f6a4419341a25c67c28
Marat Komarov <marat@scalr.com>
2014-03-14 15:06:18 +0200

    SCALARIZR-1225 wip

--- src/scalarizr/updclient/api.py
+++ src/scalarizr/updclient/api.py
@@ -363,7 +363,7 @@ class UpdClientAPI(object):
         
 
     def _ensure_repos(self, updatedb=True):
-        if 'release/latest' in self.repo_url or 'release/stable' in self.repo_url:
+        if 'release-latest' in self.repo_url or 'release-stable' in self.repo_url:
             LOG.warn("Special branches release/latest and release/stable currently doesn't work") 
             self.repo_url = devel_repo_url_for_branch('master')
         repo = pkgmgr.repository('scalr-{0}'.format(self.repository), self.repo_url)
