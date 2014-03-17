01cfb4378b310d6209056b814ce74e6b1f12a08d
Roma Koshel <roman@scalr.com>
2014-03-17 16:16:12 +0200

    fix cloudstack-python-client

--- omnibus/config/software/cloudstack-python-client.rb
+++ omnibus/config/software/cloudstack-python-client.rb
@@ -1,9 +1,9 @@
 name "cloudstack-python-client"
-version "0.2.2"
+version "0.2.3"
 
 dependency "python"
 
-source = "git+git@github.com:Scalr/python-cloudstack.git"
+source = "git+git://github.com/Scalr/python-cloudstack.git@#{version}"
 
 build do
   command "#{install_dir}/embedded/bin/pip install -I --build #{project_dir} #{source}"
