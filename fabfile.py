a0194faf08be496230c0f48d7c9edfd21fcbb045
Roma Koshel <roman@scalr.com>
2014-03-18 16:54:22 +0200

    wip

--- fabfile.py
+++ fabfile.py
@@ -3,9 +3,18 @@ import os
 from fabric.api import env, cd, local, run, put, get
 from fabric.context_managers import shell_env
 
+project = os.environ['FAB_PROJECT']
 
-build = os.environ["PWD"].split('-')[-1]
-build_dir = os.path.join('/root/ci/build', build)
+build = os.environ['PWD'].split('-')[-1]
+build_dir = os.environ['PWD']
+
+version = local("git describe --tag", capture=True)
+branch = local("git rev-parse --abbrev-ref HEAD", capture=True)
+
+omnibus_dir = os.path.join(build_dir, 'omnibus')
+omnibus_build_version = '%s.b%s' % (version, build[0:8])
+
+artifacts_dir = os.environ['ARTIFACTS_DIR']
 
 
 def git_export():
@@ -22,37 +31,39 @@ def git_export():
 
 
 def build_omnibus():
-    omnibus_dir = os.path.join(build_dir, 'omnibus')
-    version = local("git describe --tag", capture=True)
-    omnibus_build_version = '%s.b%s' % (version, build[0:8])
+    # bump version
+    with cd(build_dir):
+        run("echo '%s' >src/%s/version" % (project, version))
+
+    # build
     with cd(omnibus_dir):
         run("bundle install --binstubs")
         with shell_env(BUILD_DIR=build_dir, OMNIBUS_BUILD_VERSION=omnibus_build_version):
-            run("bin/omnibus build project scalarizr")
+            run("bin/omnibus build project %s" % project)
 
 
 def build_source():
     git_export()
-    version = local("git describe --tag", capture=True)
+
     with cd(build_dir):
         # bump version
-        run("echo '%s' >src/scalarizr/version" % version)
+        run("echo '%s' >src/%s/version" % (version, project))
         # build
         run("python setup.py sdist")
-    local("mkdir /root/ci/artifacts/%s" % build)
-    get('%s/dist/*.tar.gz' % build_dir, '/root/ci/artifacts/%s' % build)
+
+    local("mkdir -p %s" % os.path.join(artifacts_dir, project, branch, build))
+    get(
+        '%s/dist/*.tar.gz' % build_dir,
+        os.path.join(artifacts_dir, project, branch, build)
+    )
 
 
 def build_binary():
     git_export()
     build_omnibus()
-    version = local("git describe --tag", capture=True)
-    omnibus_build_version = '%s.b%s' % (version, build[0:8])
+
+    local("mkdir -p %s" % os.path.join(artifacts_dir, project, branch, build))
     files = run("ls %s/omnibus/pkg/*%s*" % (build_dir, omnibus_build_version)).split()
     for f in files:
-        get(f, '/root/ci/repos/%s' % os.path.basename(f))
+        get(f, os.path.join(artifacts_dir, project, branch, build)
         run('rm -f /var/cache/omnibus/pkg/%s' % os.path.basename(f))
-
-
-def clean():
-    run("rm -rf %s" % build_dir)
