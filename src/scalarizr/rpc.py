e9c49caaadf80445e730e00263fec039697ccaa4
Gennadiy Stas <uty@scalr.com>
2014-03-21 14:23:14 +0200

    fixed sercret global variables appearing in Task-Scheduler logs

--- src/scalarizr/rpc.py
+++ src/scalarizr/rpc.py
@@ -20,6 +20,7 @@ import socket
 import traceback
 from threading import local
 import logging
+from copy import deepcopy
 
 
 from scalarizr import util
@@ -88,6 +89,7 @@ class InternalError(ServiceError):
 
 
 class RequestHandler(object):
+
     def __init__(self, services):
         svs = None
         if not services:
@@ -99,6 +101,25 @@ class RequestHandler(object):
             self.services = {None: svs}
         else:
             self.services = services
+
+    def _clear_request_data(self, data):
+        clear_data = deepcopy(data)
+        try:
+            glob_vars = clear_data['params']['global_variables']
+            i = 0
+            for _ in xrange(len(glob_vars)):
+                var = glob_vars[i]
+                is_private = var.get('private', None)
+                if is_private != None:
+                    if int(is_private) == 1:
+                        del glob_vars[i]
+                        i -= 1
+                    elif int(is_private) == 0:
+                        del var['private']
+                i += 1
+        except KeyError:
+            pass
+        return clear_data
 
     def handle_error(self):
         LOG.exception('Caught exception')
@@ -113,6 +134,7 @@ class RequestHandler(object):
             fn = self._find_method(svs, method)
             if fn._jsonrpc == 'command':
                 log_it = True
+                data_to_log = self._clear_request_data(data)
                 LOG.debug('request: %s', json.dumps(data))
             result = self._invoke_method(fn, params)
             # important to test json serializarion before serialize the whole result
