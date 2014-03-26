58af02e3243f1901df75590ae009b4a1a99b3446
Gennadiy Stas <uty@scalr.com>
2014-03-26 14:22:28 +0200

    fixed queryenv stuck if calling unsupported method

--- src/scalarizr/queryenv.py
+++ src/scalarizr/queryenv.py
@@ -84,8 +84,12 @@ class QueryEnvService(object):
             except:
             	e = sys.exc_info()[1]
                 if isinstance(e, urllib2.HTTPError):
-                    resp_body = e.read() if e.fp is not None else ""
-                    self._logger.warn('QueryEnv failed. HTTP %s. %s. %s', e.code, resp_body or e.msg, msg_wait)
+                    resp_body = e.read() if e.fp is not None else None
+                    if not resp_body:
+                        resp_body = e.msg
+                    self._logger.warn('QueryEnv failed. HTTP %s. %s. %s', e.code, resp_body, msg_wait)
+                    if 'not supported' in resp_body:
+                        raise
                 else:
                     self._logger.warn('QueryEnv failed. %s. %s', e, msg_wait)
                 self._logger.warn('Sleep %s seconds before next attempt...', wait_seconds)
