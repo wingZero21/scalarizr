cee931ad7f4baa6f53f39c21e1922287f0bc0b38
Nick Demyanchuk <spike@scalr.com>
2014-05-20 16:34:03 +0400

    mdadm conf path fixed SCALARIZR-1447

--- src/scalarizr/storage2/volumes/raid.py
+++ src/scalarizr/storage2/volumes/raid.py
@@ -68,7 +68,7 @@ class RaidVolume(base.Volume):
 
     def _disable_autoassembly(self):
         try:
-            mdadm_conf_path = '/etc/mdadm.conf' if os_detect.rhel_family else '/etc/mdadm/mdadm.conf'
+            mdadm_conf_path = '/etc/mdadm.conf' if os_detect.redhat_family else '/etc/mdadm/mdadm.conf'
             with open(mdadm_conf_path) as f:
                 mdadm_conf = f.read()
             # TODO: augeas candidate
