63b7ed358c7f0cd46f4eea02b1d47046efb52f24
ViktorOgnev <v.ognev@scalr.com>
2014-05-26 16:12:43 +0400

    fix/SCALARIZR-1065: set buildver explicitly

--- omnibus/config/projects/scalarizr.rb
+++ omnibus/config/projects/scalarizr.rb
@@ -8,7 +8,7 @@ install_path    "/opt/scalarizr"
 if ENV['OMNIBUS_BUILD_VERSION']
     build_version   ENV['OMNIBUS_BUILD_VERSION']
 else
-    build_version   Omnibus::BuildVersion.new.semver
+    build_version   1 #Omnibus::BuildVersion.new.semver
 end
 build_iteration 1
 
