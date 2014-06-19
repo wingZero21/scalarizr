754bfd99ccc274d7ef48a5382b31f77cf088f967
Gennadiy Stas <uty@scalr.com>
2014-06-19 18:33:31 +0400

    typo fixed

--- src/scalarizr/handlers/script_executor.py
+++ src/scalarizr/handlers/script_executor.py
@@ -199,7 +199,7 @@ class ScriptExecutor(Handler):
             
         script_result = script.get_result()
         LOG.debug('script result: %s' % script_result)
-        r
+        
         if exc_info:
             LOG.debug('exception was occured :%s' % exc_info)
             # with open(script.stderr_path, 'w+') as stderr_log:
