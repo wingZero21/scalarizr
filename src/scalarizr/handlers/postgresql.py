78c41f8b24b30c329f35beda7ac9a47ddb485220
Dmytro Korsakov <dmitry@scalr.com>
2014-07-25 17:00:20 +0400

    growtest4

--- src/scalarizr/handlers/postgresql.py
+++ src/scalarizr/handlers/postgresql.py
@@ -354,7 +354,7 @@ class PostgreSqlHander(ServiceCtlHandler):
 
             #test
             LOG.info('Data volume size: %s' % postgresql_data['volume'].size)
-            postgresql_data["growth"] = int(postgresql_data['volume']['size']) + 2
+            postgresql_data["growth"] = int(postgresql_data['volume'].size) + 2
             LOG.info("Growth for the test: %s" % postgresql_data["growth"])
 
 
