503aba4c283f928142c628d499cd576f2a886352
ViktorOgnev <v.ognev@scalr.com>
2014-08-01 14:11:16 +0400

    fix/SCALARIZR-1065: fix pip name typo

--- omnibus/config/software/python-m2crypto.rb
+++ omnibus/config/software/python-m2crypto.rb
@@ -7,6 +7,6 @@ default_version "0.22.3"
 dependency "pip"
 
 build do
-    command "#{install_dir}/embedded/bin/pip3 install -I #{pypi_name}==#{default_version}"
+    command "#{install_dir}/embedded/bin/pip install -I #{pypi_name}==#{default_version}"
 end
 
