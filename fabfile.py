07abeaea2cf6fac510add44487324d099871f9fd
ViktorOgnev <v.ognev@scalr.com>
2014-08-13 16:41:48 +0400

    fix/SCALARIZR-1065: inline shopt call would not work

--- fabfile.py
+++ fabfile.py
@@ -362,7 +362,8 @@ def build_and_publish_binary():
         publish_binary()
     finally:
         run('rm -rf /root/.strider/data/scalr-int-scalarizr-*')
-        run('shopt -s extglob; rm -rf /tmp/!(vagrant-chef-1)')
+        run('shopt -s extglob;')
+        run('rm -rf /tmp/!(vagrant-chef-1)')
         time_delta = time.time() - time0
         print_green('build_and_publish_binary took {0} minutes '.format(time_delta / 60))
 
