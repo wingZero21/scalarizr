617dcb1622da741e77300ddd268fa0ed33d70b81
Marat Komarov <marat@scalr.com>
2014-08-14 15:21:24 +0300

    Install scalarizr entry points into /opt/scalarizr/bin

--- omnibus/config/software/scalarizr.rb
+++ omnibus/config/software/scalarizr.rb
@@ -6,5 +6,7 @@ source :path => ENV['BUILD_DIR']
 
 build do
   FileUtils.mkdir_p("/etc/scalr")
-  command "#{install_dir}/embedded/bin/python setup.py install --prefix=#{install_dir}/embedded"
+  command "#{install_dir}/embedded/bin/python setup.py install " \
+            "--prefix=#{install_dir}/embedded " \
+            "--install-scripts #{install_dir}/bin"
 end
