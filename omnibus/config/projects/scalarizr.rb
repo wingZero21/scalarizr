d013b7b740bc73cfad7f467ede9b4e71fea5d7f4
Roma Koshel <roman@scalr.com>
2014-08-26 14:27:49 +0400

    [SCALARIZR-1401] add WiX upgrade code

--- omnibus/config/projects/scalarizr.rb
+++ omnibus/config/projects/scalarizr.rb
@@ -12,6 +12,12 @@ else
 end
 build_iteration 1
 
+if windows?
+  package :msi do
+    upgrade_code 'c8a28fde-f340-423f-b7db-ff1368d8bb19'
+  end
+end
+
 dependency "preparation"
 dependency "scalarizr"
 dependency "version-manifest"
