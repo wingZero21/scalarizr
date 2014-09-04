c257899d1d0fff55162667239bbca152cedd8ad5
ViktorOgnev <v.ognev@scalr.com>
2014-09-04 13:19:13 +0400

    fix/SCALARIZR-1401: install openssl and m2crypto for all platfamilies

--- omnibus/config/software/scalarizr-deps.rb
+++ omnibus/config/software/scalarizr-deps.rb
@@ -33,8 +33,5 @@ dependency "python-pyyaml"
 dependency "cloudstack-python-client"
 dependency "python-pymongo"
 dependency "python-docopt"
+dependency "python-openssl"
 dependency "python-m2crypto"
-
-if linux?
-  dependency "python-openssl"
-end
