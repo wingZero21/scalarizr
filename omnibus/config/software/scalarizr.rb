7bae0ae3d089891c171ea32642ac96d2d759f17e
ViktorOgnev <v.ognev@scalr.com>
2014-11-06 16:35:28 +0300

    revert previous commit

--- omnibus/config/software/scalarizr.rb
+++ omnibus/config/software/scalarizr.rb
@@ -7,7 +7,6 @@ source :path => ENV['BUILD_DIR']
 build do
   command "#{install_dir}/embedded/bin/python setup_omnibus.py install " \
             "--prefix=#{install_dir}/embedded " \
-            "--install-scripts #{install_dir}/bin " \
-            "--single-version-externally-managed"
+            "--install-scripts #{install_dir}/bin"
   command "sed -i 's/\\#\\!\\/usr\\/bin\\/python/\\#\\!\\/opt\\/scalarizr\\/embedded\\/bin\\/python/1' #{install_dir}/scripts/*"
 end
