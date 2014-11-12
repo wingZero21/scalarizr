994f0e08d847e98a258047804b539a398e6d4cd5
Gennadiy Stas <uty@scalr.com>
2014-11-12 20:43:18 +0200

    fixed initd call parms type

--- src/scalarizr/api/nginx.py
+++ src/scalarizr/api/nginx.py
@@ -58,7 +58,7 @@ class NginxInitScript(initdv2.ParametrizedInitScript):
 
     def __init__(self):
         self._nginx_binary = __nginx__['binary_path']
-        service_call = ('service', 'nginx') if __nginx__['service_name'] == 'nginx' \
+        service_call = ['service', 'nginx'] if __nginx__['service_name'] == 'nginx' \
             else __nginx__['service_name']
         initdv2.ParametrizedInitScript.__init__(self,
                                                 'nginx',
