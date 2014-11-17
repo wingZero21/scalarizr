56be2733d7830f36886b08bfdbd84919afaaab6c
Roma Koshel <roman@scalr.com>
2014-11-17 15:36:49 +0300

    SCALARIZR-1656 fix

--- src/scalarizr/api/postgresql.py
+++ src/scalarizr/api/postgresql.py
@@ -363,9 +363,9 @@ class PostgreSQLAPI(BehaviorAPI):
                             
     @classmethod
     def do_check_software(cls, system_packages=None):
-        system_packages = system_packages or pkgmgr.package_mgr().list()
         os_name = linux.os['name'].lower()
         os_vers = linux.os['version']
+        requirements = None
         if os_name == 'ubuntu':
             if os_vers >= '12':
                 requirements = [
@@ -409,7 +409,7 @@ class PostgreSQLAPI(BehaviorAPI):
                 ['postgresql92-server', 'postgresql92', 'postgresql92-devel'],
                 ['postgresql-server>=9.2,<9.3', 'postgresql>=9.2,<9.3', 'postgresql-devel>=9.2,<9.3'],
             ]
-        else:
+        if requirements is None:
             raise exceptions.UnsupportedBehavior(
                     cls.behavior,
                     "Not supported on {0} os family".format(linux.os['family']))
