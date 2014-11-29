af0d64d7182ff0a19ee300ea3b8cfd32a32b82ef
Marat Komarov <marat@scalr.com>
2014-11-29 13:48:29 +0300

    SCALARIZR-1546 enable percona 5.6 on centos 7

--- src/scalarizr/api/percona.py
+++ src/scalarizr/api/percona.py
@@ -39,10 +39,15 @@ class PerconaAPI(mysql.MySQLAPI):
                 ['percona-server-server-5.5', 'percona-server-client-5.5'],
             ]
         elif linux.os.redhat_family or linux.os.oracle_family:
-            requirements = [
-                ['Percona-Server-server-51', 'Percona-Server-client-51'],
-                ['Percona-Server-server-55', 'Percona-Server-client-55'],
-            ]
+            if os_vers >= '7':
+                requirements = [
+                    ['Percona-Server-server-56', 'Percona-Server-client-56']                    
+                ]
+            else:
+                requirements = [
+                    ['Percona-Server-server-51', 'Percona-Server-client-51'],
+                    ['Percona-Server-server-55', 'Percona-Server-client-55']
+                ]
         else:
             raise exceptions.UnsupportedBehavior(
                     cls.behavior,
