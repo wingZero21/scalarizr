d35ffe5ea069b3814bfa6e0534dac44d01b6cea6
ViktorOgnev <v.ognev@scalr.com>
2014-12-10 14:19:11 +0300

    fix/SCALAIRZR-1649: add readine for unix and pygments

--- omnibus/config/software/python-pygments.rb
+++ omnibus/config/software/python-pygments.rb
@@ -1,15 +1,12 @@
-name "python-ipython"
-pypi_name = "ipython"
-default_version "2.3.0"
+name "python-pygments"
+pypi_name = "pygments"
+default_version "2.0.1"
 
 dependency "pip"
-dependency "python-pygments" # syntax higlighting
 
 if windows?
-    dependency "python-pyreadline" # win-specific autocompletion
     pip = "#{install_dir}/embedded/python/Scripts/pip.exe"
 else
-    dependency "python-readline"
     pip = "#{install_dir}/embedded/bin/pip"
 end
 
--- omnibus/config/software/python-pyreadline.rb
+++ omnibus/config/software/python-pyreadline.rb
@@ -1,10 +1,12 @@
+# this is a win-specific package
 name "python-pyreadline"
 pypi_name = "pyreadline"
 default_version "2.0"
 
 dependency "pip"
 
+pip = "#{install_dir}/embedded/python/Scripts/pip.exe"
+
 build do
-    pip = "#{install_dir}/embedded/bin/pip"
     command "#{pip} install -I #{pypi_name}==#{default_version}"
 end
