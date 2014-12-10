fbc46251595c3413188836c721577287a03c78c1
ViktorOgnev <v.ognev@scalr.com>
2014-12-10 18:05:34 +0300

    fix/SCALARIZR-1681: change augeas readline dep

--- omnibus/config/software/augeas.rb
+++ omnibus/config/software/augeas.rb
@@ -2,7 +2,7 @@ name "augeas"
 default_version "1.2.0"
 
 dependency "libxml2"
-dependency "python-readline"
+dependency "readline"
 
 source :url => "http://download.augeas.net/augeas-#{version}.tar.gz",
        :md5 => "dce2f52cbd20f72c7da48e014ad48076"
