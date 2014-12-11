0c08cdbda8b35a9e1688c619f38548e9df7814da
ViktorOgnev <v.ognev@scalr.com>
2014-12-11 14:56:48 +0300

    fix/SCALARIZR-1681: try passing healthcheck passing std flags only

--- omnibus/config/software/augeas.rb
+++ omnibus/config/software/augeas.rb
@@ -11,11 +11,6 @@ relative_path "augeas-#{version}"
 env = with_standard_compiler_flags(with_embedded_path)
 
 build do
-    block do
-        env["LIBXML_CFLAGS"] = `#{install_dir}/embedded/bin/xml2-config --cflags`.strip
-        env["LIBXML_LIBS"] = `#{install_dir}/embedded/bin/xml2-config --libs`.strip
-    end
-
     command "./configure --prefix=#{install_dir}/embedded", :env => env
     command "make -j #{max_build_jobs}", :env => env
     command "make install"
