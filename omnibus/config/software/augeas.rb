cfe05922c2ed432ebdf1fe7de48a1fe8cef347a1
ViktorOgnev <v.ognev@scalr.com>
2014-12-11 11:57:13 +0300

    fix/SCALARIZR-1681: use with_standard_compiler_flags method

--- omnibus/config/software/augeas.rb
+++ omnibus/config/software/augeas.rb
@@ -8,16 +8,12 @@ source :url => "http://download.augeas.net/augeas-#{version}.tar.gz",
        :md5 => "dce2f52cbd20f72c7da48e014ad48076"
 
 relative_path "augeas-#{version}"
+env = with_standard_compiler_flags(with_embedded_path)
 
-env = {
-  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
-}
 build do
     block do
-        env["LIBXML_CFLAGS"] = `#{install_dir}/embedded/bin/xml2-config --cflags`.strip
-        env["LIBXML_LIBS"] = `#{install_dir}/embedded/bin/xml2-config --libs`.strip
+        env["LIBXML_CFLAGS"] = "#{install_dir}/embedded/bin/xml2-config --cflags"
+        env["LIBXML_LIBS"] = "#{install_dir}/embedded/bin/xml2-config --libs"
     end
 
     command "./configure --prefix=#{install_dir}/embedded", :env => env
