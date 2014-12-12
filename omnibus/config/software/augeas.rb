2185d5e3d218053df39870a9af78696c08c1690d
ViktorOgnev <v.ognev@scalr.com>
2014-12-12 11:37:27 +0300

    fix/SCALARIZR-1681: remove libselinux dep, try passing build with new omnibus whitelist

--- omnibus/config/software/augeas.rb
+++ omnibus/config/software/augeas.rb
@@ -3,7 +3,6 @@ default_version "1.2.0"
 
 dependency "libxml2"
 dependency "readline"
-dependency "libselinux"
 
 source :url => "http://download.augeas.net/augeas-#{version}.tar.gz",
        :md5 => "dce2f52cbd20f72c7da48e014ad48076"
@@ -12,6 +11,10 @@ relative_path "augeas-#{version}"
 env = with_standard_compiler_flags(with_embedded_path)
 
 build do
+    block do
+        env["LIBXML_CFLAGS"] = `#{install_dir}/embedded/bin/xml2-config --cflags`.strip
+        env["LIBXML_LIBS"] = `#{install_dir}/embedded/bin/xml2-config --libs`.strip
+    end
     command "./configure --prefix=#{install_dir}/embedded", :env => env
     command "make -j #{max_build_jobs}", :env => env
     command "make install"
