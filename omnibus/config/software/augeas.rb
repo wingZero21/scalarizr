22bb8f1037877cc754c49e4ef60f5af167ab9c83
ViktorOgnev <v.ognev@scalr.com>
2014-12-15 19:27:03 +0300

    fix/SCALARIZR-1681: make additional whitelist files augeas-specific

--- omnibus/config/software/augeas.rb
+++ omnibus/config/software/augeas.rb
@@ -10,6 +10,17 @@ source :url => "http://download.augeas.net/augeas-#{version}.tar.gz",
 relative_path "augeas-#{version}"
 env = with_standard_compiler_flags(with_embedded_path)
 
+if rhel?
+  if _64_bit?
+    libdir = '/lib64'
+  else
+    libdir = '/lib'
+  end
+
+  whitelist_file "#{libdir}/libselinux.so.1"
+  whitelist_file "#{libdir}/libsepol.so.1"
+end
+
 build do
     block do
         env["LIBXML_CFLAGS"] = `#{install_dir}/embedded/bin/xml2-config --cflags`.strip
