e78fe6ea4b5ce85cad273019fcf32d60d1837d38
ViktorOgnev <v.ognev@scalr.com>
2014-12-16 11:38:30 +0300

    fix/SCALARIZR-1681: try passing healthcheck by using regex in whitelist_file

--- omnibus/config/software/augeas.rb
+++ omnibus/config/software/augeas.rb
@@ -4,22 +4,16 @@ default_version "1.2.0"
 dependency "libxml2"
 dependency "readline"
 
+if rhel?
+  whitelist_file(/libselinux\.so\.1/)
+  whitelist_file(/libsepol\.so\.1/)
+end
+
 source :url => "http://download.augeas.net/augeas-#{version}.tar.gz",
        :md5 => "dce2f52cbd20f72c7da48e014ad48076"
 
 relative_path "augeas-#{version}"
 env = with_standard_compiler_flags(with_embedded_path)
-
-if rhel?
-  if _64_bit?
-    libdir = '/lib64'
-  else
-    libdir = '/lib'
-  end
-
-  whitelist_file "#{libdir}/libselinux.so.1"
-  whitelist_file "#{libdir}/libsepol.so.1"
-end
 
 build do
     block do
