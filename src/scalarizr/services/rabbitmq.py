50edf0b2e6bdbb4ed2b3f9075bba3a43935210c9
Nick Demyanchuk <spike@scalr.com>
2015-01-09 13:52:23 +0300

    Lazy init script was not commited due to line ending settings of git

--- src/scalarizr/services/rabbitmq.py
+++ src/scalarizr/services/rabbitmq.py
@@ -77,7 +77,7 @@ class RabbitMQInitScript(initdv2.ParametrizedInitScript):
 
     reload = restart
 
-    def start(self, reason=None):
+    def start(self):
         log.debug('Starting RabbitMQ service ({0})'.format(reason or 'Reason unspecified'))
         hostname = RABBIT_HOSTNAME_TPL % __node__['server_index']
         nodename = NODE_HOSTNAME_TPL % hostname
